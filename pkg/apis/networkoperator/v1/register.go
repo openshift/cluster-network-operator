10ea8402e57b84a88e02caf6a1066693cb4a3fbb
Casey Callendrello <cdc@redhat.com>
2018-08-20 18:36:23 +0200

    api: first revision of configuration object

--- pkg/apis/networkoperator/v1/register.go
+++ pkg/apis/networkoperator/v1/register.go
@@ -28,7 +28,6 @@ func init() {
 func addKnownTypes(scheme *runtime.Scheme) error {
 	scheme.AddKnownTypes(SchemeGroupVersion,
 		&NetworkConfig{},
-		&NetworkConfigList{},
 	)
 	metav1.AddToGroupVersion(scheme, SchemeGroupVersion)
 	return nil
