9ce287f1680fe39da7edbe52de4d096265ca4c1c
Casey Callendrello <cdc@redhat.com>
2018-09-05 23:38:55 +0200

    hack: add basic build tooling

---
+++ hack/update-codegen.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+SCRIPT_ROOT=$(dirname ${BASH_SOURCE})/..
+
+${SCRIPT_ROOT}/vendor/k8s.io/code-generator/generate-groups.sh all \
+  github.com/openshift/openshift-network-operator/pkg/generated github.com/openshift/openshift-network-operator/pkg/apis \
+  "networkoperator.openshift.io:v1" \
+  --go-header-file ${SCRIPT_ROOT}/hack/custom-boilerplate.go.txt
