4c8f94bc3ea51b79a6cc4d37d49b5402baca1ebc
Casey Callendrello <cdc@redhat.com>
2018-10-15 18:41:28 -0400

    Convert to daemonset, add locking.
    Kube 1.11 doesn't let us run Deployments when the network is not up, so
    we need to be a Daemonset. That means we need locking, since more than
    one operator could run at a time.

--- cmd/cluster-network-operator/main.go
+++ cmd/cluster-network-operator/main.go
@@ -2,14 +2,17 @@ package main
 
 import (
 	"context"
+	"log"
 	"runtime"
 	"time"
 
 	netop "github.com/openshift/cluster-network-operator/pkg/operator"
+	"github.com/operator-framework/operator-sdk/pkg/leader"
 	sdk "github.com/operator-framework/operator-sdk/pkg/sdk"
 	sdkVersion "github.com/operator-framework/operator-sdk/version"
 
 	"github.com/sirupsen/logrus"
+	"github.com/spf13/pflag"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 )
 
@@ -22,6 +25,15 @@ func printVersion() {
 func main() {
 	printVersion()
 
+	var manifestPath string
+	pflag.StringVar(&manifestPath, "bindata", "./bindata", "directory containing network manifests")
+
+	err := leader.Become(context.Background(), "cluster-network-operator")
+	if err != nil {
+		log.Fatalf("Failed to become leader: %v", err)
+		return
+	}
+
 	//sdk.ExposeMetricsPort()
 
 	resource := "networkoperator.openshift.io/v1"
@@ -31,6 +43,6 @@ func main() {
 	resyncPeriod := time.Duration(60) * time.Second
 	logrus.Infof("Watching %s, %s, %s, %d", resource, kind, namespace, resyncPeriod)
 	sdk.Watch(resource, kind, namespace, resyncPeriod)
-	sdk.Handle(netop.MakeHandler("./bindata"))
-	sdk.Run(context.TODO())
+	sdk.Handle(netop.MakeHandler(manifestPath))
+	sdk.Run(context.Background())
 }
