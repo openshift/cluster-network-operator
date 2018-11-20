417ebfe8e87999145d672758b757e8dea580386d
Zenghui Shi <zshi@redhat.com>
2018-11-20 10:59:16 +0800

    multus: add additional network support

---
+++ pkg/network/additional_networks.go
@@ -0,0 +1,58 @@
+package network
+
+import (
+	"encoding/json"
+	netv1 "github.com/openshift/cluster-network-operator/pkg/apis/networkoperator/v1"
+	"github.com/openshift/cluster-network-operator/pkg/render"
+	"github.com/pkg/errors"
+	uns "k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"path/filepath"
+)
+
+// renderMultusConfig returns the manifests of Multus DaemonSet and NetworkAttachmentDefinition.
+func renderMultusConfig(manifestDir string) ([]*uns.Unstructured, error) {
+	objs := []*uns.Unstructured{}
+	// render the manifests on disk
+	data := render.MakeRenderData()
+	manifests, err := render.RenderDir(filepath.Join(manifestDir, "network/additional-networks/multus"), &data)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to render manifests")
+	}
+	objs = append(objs, manifests...)
+	return objs, nil
+}
+
+// renderRawCNIConfig returns the RawCNIConfig manifests
+func renderRawCNIConfig(conf *netv1.AdditionalNetworkDefinition, manifestDir string) ([]*uns.Unstructured, error) {
+	var err error
+	objs := []*uns.Unstructured{}
+
+	// render RawCNIConfig manifests
+	data := render.MakeRenderData()
+	data.Data["AdditionalNetworkName"] = conf.Name
+	data.Data["AdditionalNetworkConfig"] = conf.RawCNIConfig
+	objs, err = render.RenderDir(filepath.Join(manifestDir, "network/additional-networks/cr"), &data)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to render additional network")
+	}
+	return objs, nil
+}
+
+// validateRaw checks the AdditionalNetwork name and RawCNIConfig.
+func validateRaw(conf *netv1.AdditionalNetworkDefinition) []error {
+	out := []error{}
+	var rawConfig map[string]interface{}
+	var err error
+
+	if conf.Name == "" {
+		out = append(out, errors.Errorf("Additional Network Name cannot be nil"))
+	}
+
+	confBytes := []byte(conf.RawCNIConfig)
+	err = json.Unmarshal(confBytes, &rawConfig)
+	if err != nil {
+		out = append(out, errors.Errorf("Failed to Unmarshal RawCNIConfig: %v", confBytes))
+	}
+
+	return out
+}
