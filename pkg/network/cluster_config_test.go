c23405fc9e3aa18589785ea36ea94c472896bba8
Casey Callendrello <cdc@redhat.com>
2018-12-03 19:02:06 +0100

    Convert cluster.openshift.io/Network in to the Network CRD; update Network status
    This adds a second controller that watches the cluster-level network
    configuration. If changes are made, it merges those in to (or creates)
    the CRD configuration.
    
    When the changes are applied, the "real" controller then updates the
    Network status object.
    
    In the event that the downstream NetworkConfig object has changes that
    would be overwritten by the upstream Network object, do the overwrite
    and update the NetworkConfig object back.

---
+++ pkg/network/cluster_config_test.go
@@ -0,0 +1,123 @@
+package network
+
+import (
+	"testing"
+
+	configv1 "github.com/openshift/api/config/v1"
+	netopv1 "github.com/openshift/cluster-network-operator/pkg/apis/networkoperator/v1"
+
+	. "github.com/onsi/gomega"
+)
+
+var ClusterConfig = configv1.NetworkSpec{
+	ClusterNetwork: []configv1.ClusterNetworkEntry{
+		{
+			CIDR:       "10.0.0.0/22",
+			HostPrefix: 24,
+		},
+		{
+			CIDR:       "10.2.0.0/22",
+			HostPrefix: 23,
+		},
+	},
+	ServiceNetwork: []string{"192.168.0.0/20"},
+
+	NetworkType: "None",
+}
+
+func TestValidateClusterConfig(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	cc := *ClusterConfig.DeepCopy()
+	err := ValidateClusterConfig(cc)
+	g.Expect(err).NotTo(HaveOccurred())
+
+	haveError := func(cfg configv1.NetworkSpec, substr string) {
+		t.Helper()
+		err = ValidateClusterConfig(cc)
+		g.Expect(err).To(MatchError(ContainSubstring(substr)))
+	}
+
+	// invalid service cidr
+	cc.ServiceNetwork[0] = "123q"
+	haveError(cc, "could not parse spec.serviceNetwork 123q")
+
+	// service cidr overlap with network
+	cc.ServiceNetwork[0] = "10.0.2.0/24"
+	haveError(cc, "CIDRs 10.0.2.0/24 and 10.0.0.0/22 overlap")
+
+	// no service cidr
+	cc.ServiceNetwork = nil
+	haveError(cc, "spec.serviceNetwork must have length 1")
+
+	// valid clustercidr
+	cc = *ClusterConfig.DeepCopy()
+	cc.ClusterNetwork[0].CIDR = "1234fz"
+	haveError(cc, "could not parse spec.clusterNetwork 1234fz")
+
+	cc.ClusterNetwork[0].CIDR = "192.168.2.0/23"
+	haveError(cc, "CIDRs 192.168.0.0/20 and 192.168.2.0/23 overlap")
+
+	cc = *ClusterConfig.DeepCopy()
+	cc.ClusterNetwork[1].HostPrefix = 21
+	haveError(cc, "hostPrefix 21 is larger than its cidr 10.2.0.0/22")
+
+	// network type
+	cc = *ClusterConfig.DeepCopy()
+	cc.NetworkType = ""
+	haveError(cc, "spec.networkType is required")
+}
+
+func TestMergeClusterConfig(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	cc := *ClusterConfig.DeepCopy()
+
+	oc := netopv1.NetworkConfigSpec{}
+
+	MergeClusterConfig(&oc, cc)
+	g.Expect(oc).To(Equal(netopv1.NetworkConfigSpec{
+		ServiceNetwork: "192.168.0.0/20",
+		ClusterNetworks: []netopv1.ClusterNetwork{
+			{
+				CIDR:             "10.0.0.0/22",
+				HostSubnetLength: 8,
+			},
+			{
+				CIDR:             "10.2.0.0/22",
+				HostSubnetLength: 9,
+			},
+		},
+		DefaultNetwork: netopv1.DefaultNetworkDefinition{
+			Type: "None",
+		},
+	}))
+}
+
+func TestStatusFromConfig(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	crd := OpenShiftSDNConfig.DeepCopy()
+	FillDefaults(&crd.Spec, nil)
+
+	var mtu uint32 = 1300
+	crd.Spec.DefaultNetwork.OpenShiftSDNConfig.MTU = &mtu
+
+	status := StatusFromOperatorConfig(&crd.Spec)
+	g.Expect(status).To(Equal(configv1.NetworkStatus{
+		ClusterNetwork: []configv1.ClusterNetworkEntry{
+			{
+				CIDR:       "10.128.0.0/15",
+				HostPrefix: 23,
+			},
+			{
+				CIDR:       "10.0.0.0/14",
+				HostPrefix: 24,
+			},
+		},
+		ServiceNetwork:    []string{"172.30.0.0/16"},
+		ClusterNetworkMTU: 1300,
+
+		NetworkType: "OpenShiftSDN",
+	}))
+}
---
+++ pkg/util/ip/addr_test.go
@@ -0,0 +1,123 @@
+package ip
+
+import (
+	"net"
+	"testing"
+
+	. "github.com/onsi/gomega"
+)
+
+func TestAddrPool(t *testing.T) {
+	g := NewGomegaWithT(t)
+	testcases := []struct {
+		cidr string
+		ok   bool
+	}{
+		{
+			"10.0.0.0/24",
+			true,
+		},
+		{
+			"10.0.0.0/24",
+			false,
+		},
+		{
+			"10.0.2.0/24",
+			true,
+		},
+		{
+			"fe80:1:2:3::/64",
+			true,
+		},
+		{
+			"fe80:1:2:3:4::/80",
+			false,
+		},
+	}
+
+	pool := IPPool{}
+	for idx, tc := range testcases {
+		_, cidr, err := net.ParseCIDR(tc.cidr)
+		g.Expect(err).NotTo(HaveOccurred())
+		err = pool.Add(*cidr)
+		if tc.ok {
+			g.Expect(err).NotTo(HaveOccurred(), "tc %d", idx)
+		} else {
+			g.Expect(err).To(HaveOccurred(), "tc %d", idx)
+		}
+	}
+}
+
+func TestNetsOverlap(t *testing.T) {
+	g := NewGomegaWithT(t)
+	testcases := []struct {
+		cidr1    string
+		cidr2    string
+		expected bool
+	}{
+		{
+			"10.0.0.0/24",
+			"10.0.1.0/24",
+			false,
+		},
+		//
+		{
+			"10.0.0.0/22",
+			"10.0.0.0/24",
+			true,
+		},
+		{
+			"10.0.0.0/24",
+			"10.0.0.0/22",
+			true,
+		},
+		{
+			"10.0.0.0/22",
+			"10.0.3.0/24",
+			true,
+		},
+		{
+			"fe80:1:2:3::/64",
+			"fe80:1:2:3:4::/80",
+			true,
+		},
+	}
+
+	for _, tc := range testcases {
+		_, c1, err := net.ParseCIDR(tc.cidr1)
+		g.Expect(err).NotTo(HaveOccurred())
+		_, c2, err := net.ParseCIDR(tc.cidr2)
+		g.Expect(err).NotTo(HaveOccurred())
+
+		g.Expect(netsOverlap(*c1, *c2)).To(Equal(tc.expected))
+
+	}
+}
+
+func TestLastIP(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	testcases := []struct {
+		cidr     string
+		expected string
+	}{
+		{
+			"10.0.0.0/24",
+			"10.0.0.255",
+		},
+		{
+			"10.0.0.128/30",
+			"10.0.0.131",
+		},
+		{
+			"fe80:1:2:3::/64",
+			"fe80:1:2:3:ffff:ffff:ffff:ffff",
+		},
+	}
+
+	for _, tc := range testcases {
+		_, cidr, err := net.ParseCIDR(tc.cidr)
+		g.Expect(err).NotTo(HaveOccurred())
+		g.Expect(lastIP(*cidr).String()).To(Equal(tc.expected))
+	}
+}
