743c6940b5e201ae7e2967c0cf8363b568f9ea4b
Dan Williams <dcbw@redhat.com>
2019-01-04 11:39:08 -0600

    hack: use local GOPATH if GOPATH isn't set

--- hack/build-go.sh
+++ hack/build-go.sh
@@ -2,35 +2,26 @@
 
 set -eu
 
-REPO=github.com/openshift/cluster-network-operator
+source "$(dirname "${BASH_SOURCE}")/init.sh"
+setup_env
+
 CMDS=${CMDS:-cluster-network-operator cluster-network-renderer}
 GOFLAGS=${GOFLAGS:-}
 GLDFLAGS=${GLDFLAGS:-}
 
-eval $(go env | grep -e "GOHOSTOS" -e "GOHOSTARCH")
-
-GOOS=${GOOS:-${GOHOSTOS}}
-GOARCH=${GOACH:-${GOHOSTARCH}}
-
 # Go to the root of the repo
-cd "$(git rev-parse --show-cdup)"
+cd "${CNO_ROOT}"
 
 if [ -z ${VERSION+a} ]; then
 	echo "Using version from git..."
 	VERSION=$(git describe --abbrev=8 --dirty --always)
 fi
 
-GLDFLAGS+="-X ${REPO}/pkg/version.Raw=${VERSION}"
+GLDFLAGS+="-X ${CNO_GO_PKG}/pkg/version.Raw=${VERSION}"
 
 eval $(go env)
 
-if [ -z ${BIN_PATH+a} ]; then
-	export BIN_PATH=_output/${GOOS}/${GOARCH}
-fi
-
-mkdir -p ${BIN_PATH}
-
 for cmd in ${CMDS}; do
-	echo "Building ${REPO}/cmd/${cmd} (${VERSION})"
-	CGO_ENABLED=0 GOOS=${GOOS} GOARCH=${GOARCH} go build ${GOFLAGS} -ldflags "${GLDFLAGS}" -o ${BIN_PATH}/${cmd} ${REPO}/cmd/${cmd}
+	echo "Building ${CNO_GO_PKG}/cmd/${cmd} (${VERSION})"
+	CGO_ENABLED=0 GOOS=${GOOS} GOARCH=${GOARCH} go build ${GOFLAGS} -ldflags "${GLDFLAGS}" -o ${CNO_BIN_PATH}/${cmd} ${CNO_GO_PKG}/cmd/${cmd}
 done
