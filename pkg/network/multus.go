6ac99b06695353538cc884531cb1d7aab9248b81
Dan Williams <dcbw@redhat.com>
2019-01-10 20:30:01 -0600

    cni: add supported & unsupported plugins

--- pkg/network/multus.go
+++ pkg/network/multus.go
@@ -15,6 +15,8 @@ func renderMultusConfig(manifestDir string) ([]*uns.Unstructured, error) {
 	// render the manifests on disk
 	data := render.MakeRenderData()
 	data.Data["MultusImage"] = os.Getenv("MULTUS_IMAGE")
+	data.Data["CNIPluginsSupportedImage"] = os.Getenv("CNI_PLUGINS_SUPPORTED_IMAGE")
+	data.Data["CNIPluginsUnsupportedImage"] = os.Getenv("CNI_PLUGINS_UNSUPPORTED_IMAGE")
 
 	manifests, err := render.RenderDir(filepath.Join(manifestDir, "network/multus"), &data)
 	if err != nil {
