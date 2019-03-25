c36bd1ad1c5b613cf94120bcb66d3ff977d094cd
Dan Winship <danw@redhat.com>
2019-03-25 11:33:26 -0400

    run-locally.sh: Drop the separate prepare/start steps
    If the user hasn't run "openshift-install create manifests" yet, then
    print an error. If they have run "create manifests" but not "create
    cluster", and we haven't inserted our overrides yet, then do that.
    Otherwise, wait for the cluster to come up and then start the
    operator.

--- hack/run-locally.sh
+++ hack/run-locally.sh
@@ -7,19 +7,18 @@
 set -o errexit
 set -o nounset
 
+if [[ -n "${HACK_MINIMIZE:-}" ]]; then
+    echo "HACK_MINIMIZE set! This is only for development, and the installer will likely not succeed."
+    echo "You will still be left with a reasonably functional cluster."
+    echo ""
+fi
+
 # Install our overrides so the cluster doesn't run the network operator.
 function override_install_manifests() {
-    if [[ ! -e "${CLUSTER_DIR}/manifests/cvo-overrides.yaml" ]]; then
-        echo "cannot find cvo-overrides.yaml; please run"
-        echo "openshift-install --dir=${CLUSTER_DIR} create manifests"
-        exit 1
-    fi
-
     # Patch the CVO to not create the network operator
     kubectl --kubeconfig=hack/null-kubeconfig patch --type=json --local=true -f="${CLUSTER_DIR}/manifests/cvo-overrides.yaml" -p "$(cat hack/overrides-patch.yaml)" -o yaml > "${CLUSTER_DIR}/manifests/.cvo-overrides.yaml.new"
     # Optionally, tell the CVO to skip some unnecessary components
     if [[ -n "${HACK_MINIMIZE:-}" ]]; then
-        echo "HACK_MINIMIZE set! This is only for rapid development!"
         kubectl --kubeconfig=hack/null-kubeconfig patch --type=json --local=true -f="${CLUSTER_DIR}/manifests/.cvo-overrides.yaml.new" -p "$(cat hack/overrides-minimize-patch.yaml)" -o yaml > "${CLUSTER_DIR}/manifests/.cvo-overrides.yaml.new.2"
         mv "${CLUSTER_DIR}/manifests/.cvo-overrides.yaml.new.2" "${CLUSTER_DIR}/manifests/.cvo-overrides.yaml.new"
     fi
@@ -40,14 +39,8 @@ function setup_operator_env() {
     fi
 }
 
-# start_operator waits for the cluster to come up, then launches
-# the operator locally.
-function start_operator() {
-    if [[ -n "${HACK_MINIMIZE:-}" ]]; then
-        echo "HACK_MINIMIZE set! This is only for development, and the installer will likely not succeed."
-        echo "You will still be left with a reasonably functional cluster."
-    fi
-
+# wait_for_cluster waits for the cluster to come up
+function wait_for_cluster() {
     export KUBECONFIG="${CLUSTER_DIR}/auth/kubeconfig"
 
     if [[ ! -e "${KUBECONFIG}" ]]; then
@@ -85,50 +78,29 @@ function start_operator() {
             sleep 5
         done
         echo "Found network operator configuration"
+        echo ""
+        echo "Ready to start operator. (If this fails, try running '$0' again.)"
     fi
-
-    echo ""
-    echo "Starting operator. (If this fails, try running '$0 start' again.)"
-    env $(cat "${CLUSTER_DIR}/env.sh") _output/linux/amd64/cluster-network-operator
-}
-
-function usage() {
-    >&2 echo "Usage: $0 (prepare|start)
-
-Scaffolding for running a local build of the network operator with the installer.
-For more info, see INSTALLER-HACKING.md
-
-Commands:
-- prepare: adds overrides to prevent the default network operator from starting, extracts image references
-- start: Starts the local network operator
-
-Required environment variables:
-CLUSTER_DIR - The state directory used by the installer.
-"
 }
 
 if [[ -z "${CLUSTER_DIR:-}" ]]; then
-    echo error: CLUSTER_DIR is required
-    echo
-    usage
+    echo "error: CLUSTER_DIR must be set"
+    echo "For more info, see INSTALLER-HACKING.md"
     exit 1
 fi
 
-case "${1:-""}" in
-    prepare)
-        override_install_manifests;
-        setup_operator_env;
-        ;;
-    start)
-        if [[ ! -e "${CLUSTER_DIR}/env.sh" ]]; then
-            echo "env.sh missing, run prepare first"
-            exit 1
-        fi
+if [[ ! -e "${CLUSTER_DIR}/env.sh" && ! -e "${CLUSTER_DIR}/manifests/cvo-overrides.yaml" ]]; then
+    echo "error: cannot find installer state; please run"
+    echo "  openshift-install --dir=${CLUSTER_DIR} create manifests"
+    echo "For more info, see INSTALLER-HACKING.md"
+    exit 1
+fi
 
-        start_operator;
-        ;;
-    *)
-        usage
-        exit 1
-        ;;
-esac
+if [[ -e "${CLUSTER_DIR}/manifests/cvo-overrides.yaml" ]]; then
+    override_install_manifests
+fi
+
+setup_operator_env
+wait_for_cluster
+
+env $(cat "${CLUSTER_DIR}/env.sh") _output/linux/amd64/cluster-network-operator
