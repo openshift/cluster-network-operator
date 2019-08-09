da53f873d386ec6f10a788b99705145c43b8e5e0
Daneyon Hansen <daneyonhansen@gmail.com>
2019-08-08 23:39:46 -0700

    Adds Trust Bundle Publishing to Proxy Controller

--- pkg/controller/proxyconfig/validation.go
+++ pkg/controller/proxyconfig/validation.go
@@ -1,7 +1,11 @@
 package proxyconfig
 
 import (
+	"context"
+	"crypto/x509"
 	"fmt"
+	"io/ioutil"
+	"k8s.io/apimachinery/pkg/types"
 	"net"
 	"net/http"
 	"net/url"
@@ -9,23 +13,27 @@ import (
 	"time"
 
 	configv1 "github.com/openshift/api/config/v1"
+	"github.com/openshift/cluster-network-operator/pkg/names"
 	"github.com/openshift/cluster-network-operator/pkg/util/validation"
+
+	corev1 "k8s.io/api/core/v1"
 )
 
 const (
 	proxyHTTPScheme  = "http"
 	proxyHTTPSScheme = "https"
+	// noProxyWildcard is the string used to as a wildcard attached to a
+	// domain suffix in proxy.spec.noProxy to bypass proxying.
+	noProxyWildcard = "*"
 	// proxyProbeMaxRetries is the number of times to attempt an http GET
 	// to a readinessEndpoints endpoint.
 	proxyProbeMaxRetries = 3
 	// proxyProbeWaitTime is the time to wait before retrying a failed proxy probe.
 	proxyProbeWaitTime = 1 * time.Second
-	// noProxyWildcard is the string used to as a wildcard attached to a
-	// domain suffix in proxy.spec.noProxy to bypass proxying.
-	noProxyWildcard = "*"
 )
 
-// ValidateProxyConfig ensures that proxyConfig is valid.
+// ValidateProxyConfig ensures that httpProxy, httpsProxy and
+// noProxy fields of proxyConfig are valid.
 func (r *ReconcileProxyConfig) ValidateProxyConfig(proxyConfig *configv1.ProxySpec) error {
 	if isSpecHTTPProxySet(proxyConfig) {
 		scheme, err := validation.URI(proxyConfig.HTTPProxy)
@@ -80,6 +88,75 @@ func (r *ReconcileProxyConfig) ValidateProxyConfig(proxyConfig *configv1.ProxySp
 	}
 
 	return nil
+}
+
+// validateTrustedCA validates that trustedCA is a valid ConfigMap
+// reference and that the ConfigMap contains a valid trust bundle,
+// returning the byte slices of the certificate data from the
+// validated trustedCA and system trust bundles.
+func (r *ReconcileProxyConfig) validateTrustedCA(trustedCA string) ([]byte, []byte, error) {
+	cfgMap, err := r.validateConfigMapRef(trustedCA)
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to validate configmap reference for proxy trustedCA '%s': %v",
+			trustedCA, err)
+	}
+
+	// TODO: Update return values to include []*x509.Certificates for https readinessEndpoint support.
+	_, bundleData, err := r.validateTrustBundle(cfgMap)
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to validate trust bundle for proxy trustedCA '%s': %v",
+			trustedCA, err)
+	}
+
+	systemData, err := r.validateSystemTrustBundle(names.SYSTEM_TRUST_BUNDLE)
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to validate system trust bundle '%s': %v", names.SYSTEM_TRUST_BUNDLE, err)
+	}
+
+	return bundleData, systemData, nil
+}
+
+// validateConfigMapRef validates that trustedCA is a valid ConfigMap reference,
+// returning the validated ConfigMap.
+func (r *ReconcileProxyConfig) validateConfigMapRef(trustedCA string) (*corev1.ConfigMap, error) {
+	cfgMap := &corev1.ConfigMap{}
+	ns := names.ADDL_TRUST_BUNDLE_CONFIGMAP_NS
+	if trustedCA == names.TRUSTED_CA_BUNDLE_CONFIGMAP {
+		ns = names.TRUSTED_CA_BUNDLE_CONFIGMAP_NS
+	}
+	if err := r.client.Get(context.TODO(), types.NamespacedName{Namespace: ns, Name: trustedCA}, cfgMap); err != nil {
+		return nil, fmt.Errorf("failed to get trustedCA configmap for proxy %s: %v", names.PROXY_CONFIG, err)
+	}
+
+	return cfgMap, nil
+}
+
+// validateTrustBundle is a wrapper for validation.TrustBundleConfigMap(), which
+// validates that cfgMap contains a data key named "ca-bundle.crt" and the value
+// of the key is one or more valid PEM encoded certificates, returning slices of
+// the validated certificates and certificate data.
+func (r *ReconcileProxyConfig) validateTrustBundle(cfgMap *corev1.ConfigMap) ([]*x509.Certificate, []byte, error) {
+	certBundle, bundleData, err := validation.TrustBundleConfigMap(cfgMap)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return certBundle, bundleData, nil
+}
+
+// validateSystemTrustBundle reads the trustBundle file, ensuring each
+// PEM block is type "CERTIFICATE" and the block can be parsed as an
+// x509 CA certificate, returning the parsed certificates as a []byte.
+func (r *ReconcileProxyConfig) validateSystemTrustBundle(trustBundle string) ([]byte, error) {
+	bundleData, err := ioutil.ReadFile(trustBundle)
+	if err != nil {
+		return nil, err
+	}
+	if _, _, err := validation.CertificateData(bundleData); err != nil {
+		return nil, err
+	}
+
+	return bundleData, nil
 }
 
 // validateHTTPReadinessEndpoint validates an http readinessEndpoint endpoint.
