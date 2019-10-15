bbf484963084c81c7d508c94b69a7b56365709c8
Casey Callendrello <cdc@redhat.com>
2019-10-15 17:36:31 +0200

    Add simple OperatorPKI controller.
    This uses the existing PKI implementation in library-go, wiring it up to
    the OperatorPKI CRD.
    
    Creating a OperatorPKI CR will create a CA and a single certificate.

--- pkg/controller/add_networkconfig.go
+++ pkg/controller/add_networkconfig.go
@@ -2,14 +2,16 @@ package controller
 
 import (
 	"github.com/openshift/cluster-network-operator/pkg/controller/clusterconfig"
-	"github.com/openshift/cluster-network-operator/pkg/controller/configmap_ca_injector"
+	configmapcainjector "github.com/openshift/cluster-network-operator/pkg/controller/configmap_ca_injector"
 	"github.com/openshift/cluster-network-operator/pkg/controller/operconfig"
+	"github.com/openshift/cluster-network-operator/pkg/controller/pki"
 	"github.com/openshift/cluster-network-operator/pkg/controller/proxyconfig"
 )
 
 func init() {
 	// AddToManagerFuncs is a list of functions to create controllers and add them to a manager.
 	AddToManagerFuncs = append(AddToManagerFuncs,
+		pki.Add,
 		proxyconfig.Add,
 		operconfig.Add,
 		clusterconfig.Add,
