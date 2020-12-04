29a74d74d4f9da68281065678af9ad9dccbdae4d
Mark Gray <mark.d.gray@redhat.com>
2020-12-04 09:54:56 -0500

    Force enable IPsec
    Signed-off-by: Mark Gray <mark.d.gray@redhat.com>

--- pkg/network/ovn_kubernetes.go
+++ pkg/network/ovn_kubernetes.go
@@ -112,11 +112,7 @@ func renderOVNKubernetes(conf *operv1.NetworkSpec, bootstrapResult *bootstrap.Bo
 		data.Data["OVNHybridOverlayVXLANPort"] = ""
 	}
 
-	if c.IPsecConfig != nil {
-		data.Data["EnableIPsec"] = true
-	} else {
-		data.Data["EnableIPsec"] = false
-	}
+	data.Data["EnableIPsec"] = true
 
 	manifests, err := render.RenderDir(filepath.Join(manifestDir, "network/ovn-kubernetes"), &data)
 
@@ -126,19 +122,17 @@ func renderOVNKubernetes(conf *operv1.NetworkSpec, bootstrapResult *bootstrap.Bo
 
 	objs = append(objs, manifests...)
 
-	if c.IPsecConfig != nil {
-		// Only render ipsec manifest if ipsec has been enabled at cluster
-		// installation time. We will never have to delete the ipsec pod
-		// because it cannot be disabled at runtime
-		//
-		// We must render these manifests after ovn-kubernetes manifests
-		// as they create the openshift-ovn-kubernetes namespace
-		ipsecManifests, err := render.RenderDir(filepath.Join(manifestDir, "network/ovn-kubernetes-ipsec"), &data)
-		if err != nil {
-			return nil, errors.Wrap(err, "failed to render ipsec manifest")
-		}
-		objs = append(objs, ipsecManifests...)
+	// Only render ipsec manifest if ipsec has been enabled at cluster
+	// installation time. We will never have to delete the ipsec pod
+	// because it cannot be disabled at runtime
+	//
+	// We must render these manifests after ovn-kubernetes manifests
+	// as they create the openshift-ovn-kubernetes namespace
+	ipsecManifests, err := render.RenderDir(filepath.Join(manifestDir, "network/ovn-kubernetes-ipsec"), &data)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to render ipsec manifest")
 	}
+	objs = append(objs, ipsecManifests...)
 
 	return objs, nil
 }
