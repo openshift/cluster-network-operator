fd557ac3d3756d16c6144336a37c0b89f2ecab93
Juan-Luis de Sousa-Valadas Castaño <jdesousa@redhat.com>
2021-01-13 12:33:14 +0100

    TMP DNM: Horrible hack to get extra logging

--- vendor/k8s.io/klog/klog.go
+++ vendor/k8s.io/klog/klog.go
@@ -1127,30 +1127,7 @@ type Verbose bool
 func V(level Level) Verbose {
 	// This function tries hard to be cheap unless there's work to do.
 	// The fast path is two atomic loads and compares.
-
-	// Here is a cheap but safe test to see if V logging is enabled globally.
-	if logging.verbosity.get() >= level {
-		return Verbose(true)
-	}
-
-	// It's off globally but it vmodule may still be set.
-	// Here is another cheap but safe test to see if vmodule is enabled.
-	if atomic.LoadInt32(&logging.filterLength) > 0 {
-		// Now we need a proper lock to use the logging structure. The pcs field
-		// is shared so we must lock before accessing it. This is fairly expensive,
-		// but if V logging is enabled we're slow anyway.
-		logging.mu.Lock()
-		defer logging.mu.Unlock()
-		if runtime.Callers(2, logging.pcs[:]) == 0 {
-			return Verbose(false)
-		}
-		v, ok := logging.vmap[logging.pcs[0]]
-		if !ok {
-			v = logging.setV(logging.pcs[0])
-		}
-		return Verbose(v >= level)
-	}
-	return Verbose(false)
+	return Verbose(true)
 }
 
 // Info is equivalent to the global Info function, guarded by the value of v.
