4b65265c748e2133ba6694cb3fad9aa1dac5f470
Federico Paolinelli <fpaoline@redhat.com>
2021-03-17 23:09:16 +0100

    OVN Upgrade: fix upgrade order of node and master
    The current logic attempts to restore the old version of the daemonset
    if shouldUpdateOVNK returns false, but the replacement is done against a
    list that does not contain the ovnk rendered objects yet.
    
    Signed-off-by: Federico Paolinelli <fpaoline@redhat.com>

--- pkg/network/ovn_kubernetes.go
+++ pkg/network/ovn_kubernetes.go
@@ -129,6 +129,7 @@ func renderOVNKubernetes(conf *operv1.NetworkSpec, bootstrapResult *bootstrap.Bo
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to render manifests")
 	}
+	objs = append(objs, manifests...)
 
 	updateNode, updateMaster := shouldUpdateOVNK(bootstrapResult.OVN.ExistingNodeDaemonset, bootstrapResult.OVN.ExistingMasterDaemonset, os.Getenv("RELEASE_VERSION"))
 
@@ -148,7 +149,6 @@ func renderOVNKubernetes(conf *operv1.NetworkSpec, bootstrapResult *bootstrap.Bo
 		objs = k8s.ReplaceObj(objs, us)
 	}
 
-	objs = append(objs, manifests...)
 	return objs, nil
 }
 
@@ -453,7 +453,6 @@ func shouldUpdateOVNK(existingNode, existingMaster *appsv1.DaemonSet, releaseVer
 	if masterDelta == versionUnknown || nodeDelta == versionUnknown {
 		log.Printf("WARNING: could not determine ovn-kubernetes daemonset update directions; node: %s, master: %s, release: %s",
 			nodeVersion, masterVersion, releaseVersion)
-
 		return true, true
 	}
 
