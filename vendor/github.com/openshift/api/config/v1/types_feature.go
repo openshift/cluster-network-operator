52ea2a109a0cfd7fa6847ca760f3545cd7dbcf9d
Maysa Macedo <maysa.macedo95@gmail.com>
2021-04-06 18:19:50 +0000

    Bump openshift/api to include mtu in kuryrConfig
    The follwing commands were ran to update openshift/api
    vendor and remove unnecessary entries:
    - go mod tidy
    - go mod vendor

--- vendor/github.com/openshift/api/config/v1/types_feature.go
+++ vendor/github.com/openshift/api/config/v1/types_feature.go
@@ -105,7 +105,10 @@ var FeatureSets = map[FeatureSet]*FeatureGateEnabledDisabled{
 		Enabled:  []string{},
 		Disabled: []string{},
 	},
-	TechPreviewNoUpgrade: newDefaultFeatures().toFeatures(),
+	TechPreviewNoUpgrade: newDefaultFeatures().
+		with("CSIDriverAzureDisk"). // sig-storage, jsafrane
+		with("CSIDriverVSphere").   // sig-storage, jsafrane
+		toFeatures(),
 	LatencySensitive: newDefaultFeatures().
 		with(
 			"TopologyManager", // sig-pod, sjenning
