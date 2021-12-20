f372729673d588cd397baa3408f010bf0fb67d8e
Alexander Constantinescu <aconstan@redhat.com>
2021-12-20 18:53:14 +0100

    Move infrastructure bootstrap to its own package
    Signed-off-by: Alexander Constantinescu <aconstan@redhat.com>

--- pkg/platform/platform.go
+++ pkg/platform/platform.go
@@ -1,23 +1,22 @@
-package network
+package platform
 
 import (
 	"context"
 	"fmt"
 
 	configv1 "github.com/openshift/api/config/v1"
-	operv1 "github.com/openshift/api/operator/v1"
 	"github.com/openshift/cluster-network-operator/pkg/bootstrap"
 	types "k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
-func bootstrapCloud(conf *operv1.Network, kubeClient client.Client) (*bootstrap.CloudBootstrapResult, error) {
+func BootstrapInfra(kubeClient client.Client) (*bootstrap.InfraBootstrapResult, error) {
 	var platformType configv1.PlatformType
 	var platformRegion string
 
 	infraConfig := &configv1.Infrastructure{}
 	if err := kubeClient.Get(context.TODO(), types.NamespacedName{Name: "cluster"}, infraConfig); err != nil {
-		return nil, fmt.Errorf("failed to get infrastructure 'config': %v", err)
+		return nil, fmt.Errorf("failed to get infrastructure 'cluster': %v", err)
 	}
 
 	if infraConfig.Status.PlatformStatus.Type != "" {
@@ -29,9 +28,10 @@ func bootstrapCloud(conf *operv1.Network, kubeClient client.Client) (*bootstrap.
 		platformRegion = infraConfig.Status.PlatformStatus.GCP.Region
 	}
 
-	res := &bootstrap.CloudBootstrapResult{
-		PlatformType:   platformType,
-		PlatformRegion: platformRegion,
+	res := &bootstrap.InfraBootstrapResult{
+		PlatformType:         platformType,
+		PlatformRegion:       platformRegion,
+		ExternalControlPlane: infraConfig.Status.ControlPlaneTopology == configv1.ExternalTopologyMode,
 	}
 	return res, nil
 }
