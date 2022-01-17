98670b75a95c1643426664a220e5d19d0bfd9e15
Riccardo Ravaioli <rravaiol@redhat.com>
2022-01-17 17:21:42 -0500

    OVN interconnect support
    - Determine OVN interconnect zone mode by inspecting an (optional) configMap; apply the desired zone mode.
    - upgrade from non-IC to IC OVN-K by going through an intermediate step with 1-zone
    - Switch from IC single zone to IC multizone (as in upgrades) and back (not fully supported yet, for internal use only)
    
    Avoid clashes between single-zone ovnkube-master (using ports 9102, 9641, 9642, 29102) and multizone ovnkube-node (initially using ports 9103, 9105, 9102, 29102, 29103) during upgrade from 4.13 and avoid using ports reserved for the storage components, as described in https://github.com/openshift/enhancements/blob/master/dev-guide/host-port-registry.md  This caused the storage operator to never be available after installation of or upgrade to 4.14.
    
    In multizone ovnkube-node let's now have:
    - 9103, 9105, 29103 (which don't collide with single-zone ovnkube-master)
    - 9112, 9112 9113, 29113 so as to not collide with single-zone ovnkube-master
    
    Signed-off-by: Riccardo Ravaioli <rravaiol@redhat.com>

--- pkg/network/semver_test.go
+++ pkg/network/semver_test.go
@@ -60,3 +60,57 @@ func TestDirection(t *testing.T) {
 		})
 	}
 }
+
+func TestVersionComparison(t *testing.T) {
+	for idx, tc := range []struct {
+		version                    string
+		otherVersionMajor          int
+		otherVersionMinor          int
+		resultLessThanOrEqualTo    bool
+		resultGreaterThanOrEqualTo bool
+	}{
+		{
+			"4.14",
+			4, 14,
+			true, // <=
+			true, // >=
+		},
+		{
+			"4.14",
+			4, 15,
+			true,  // <=
+			false, // >=
+		},
+		{
+			"4.14",
+			4, 13,
+			false, // <=
+			true,  // >=
+		},
+		{
+			"4.14.0-0.ci.test-2023-06-14-124931-ci-ln-md7ivqb-latest",
+			4, 14,
+			true, // <=
+			true, // >=
+		},
+		{
+			"4.14.0-0.ci.test-2023-06-14-124931-ci-ln-md7ivqb-latest",
+			4, 15,
+			true,  // <=
+			false, // >=
+		},
+		{
+			"4.14.0-0.ci.test-2023-06-14-124931-ci-ln-md7ivqb-latest",
+			4, 13,
+			false, // <=
+			true,  // >=
+		},
+	} {
+		t.Run(strconv.Itoa(idx), func(t *testing.T) {
+			g := NewGomegaWithT(t)
+
+			g.Expect(isVersionLessThanOrEqualTo(tc.version, tc.otherVersionMajor, tc.otherVersionMinor)).To(Equal(tc.resultLessThanOrEqualTo))
+			g.Expect(isVersionGreaterThanOrEqualTo(tc.version, tc.otherVersionMajor, tc.otherVersionMinor)).To(Equal(tc.resultGreaterThanOrEqualTo))
+		})
+	}
+}
