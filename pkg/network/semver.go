27840dae84bd0d7888baa170cf422da75b85131d
Riccardo Ravaioli <rravaiol@redhat.com>
2022-01-17 17:21:42 -0500

    OVN interconnect support
    [work in progress!]
    
    - Determine OVN interconnect zone mode by inspecting an (optional) configMap; apply the desired zone mode.
    - upgrade from non-IC to IC OVN-K by going through an intermediate step with 1-zone
    - Switch from IC single zone to IC multizone (as in upgrades) and back (not fully supported yet, for internal use only)

--- pkg/network/semver.go
+++ pkg/network/semver.go
@@ -49,3 +49,37 @@ func compareVersions(fromVersion, toVersion string) versionChange {
 
 	return versionChange(v1.Compare(v2))
 }
+
+func isVersionGreaterThanOrEqualTo(version string, major int, minor int) bool {
+	v, err := semver.NewVersion(version)
+	if err != nil {
+		klog.Errorf("failed to parse version %s: %v", version, err)
+		return false
+	}
+	// 4.14 vs 5.13
+	// 4.14 vs 4.13
+	// 4.14 vs 3.15
+	if v.Major() > int64(major) {
+		return true
+	} else if v.Major() == int64(major) {
+		return v.Minor() >= int64(minor)
+	} else {
+		return false
+	}
+}
+
+func isVersionLessThanOrEqualTo(version string, major int, minor int) bool {
+	v, err := semver.NewVersion(version)
+	if err != nil {
+		klog.Errorf("failed to parse version %s: %v", version, err)
+		return false
+	}
+
+	if v.Major() < int64(major) {
+		return true
+	} else if v.Major() == int64(major) {
+		return v.Minor() <= int64(minor)
+	} else {
+		return false
+	}
+}
