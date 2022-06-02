0ecf30ef03b31937f4bc3b4eba85bc9ab7cc775c
nicklesimba <simha.nikhil@gmail.com>
2022-06-02 08:57:30 -0500

    Changed expected num of multus objects, and added deployment back in
    Signed-off-by: nicklesimba <simha.nikhil@gmail.com>

--- pkg/network/network_metrics_test.go
+++ pkg/network/network_metrics_test.go
@@ -49,7 +49,7 @@ func TestRenderNetworkMetricsDaemon(t *testing.T) {
 
 	// Check rendered object
 
-	g.Expect(len(objs)).To(Equal(23), "Expected 23 multus related objects")
+	g.Expect(len(objs)).To(Equal(28), "Expected 28 multus related objects")
 	g.Expect(objs).To(ContainElement(HaveKubernetesID("DaemonSet", "openshift-multus", "network-metrics-daemon")))
 	g.Expect(objs).To(ContainElement(HaveKubernetesID("Service", "openshift-multus", "network-metrics-service")))
 	g.Expect(objs).To(ContainElement(HaveKubernetesID("ClusterRole", "", "metrics-daemon-role")))
