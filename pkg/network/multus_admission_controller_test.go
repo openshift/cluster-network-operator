41f8f2c2b1dc50f67e9d3da842adb37f2240a53a
Tomofumi Hayashi <tohayash@redhat.com>
2022-07-21 22:05:15 +0900

    Configure ignored namespaces into multus-admission-controller

--- pkg/network/multus_admission_controller_test.go
+++ pkg/network/multus_admission_controller_test.go
@@ -6,6 +6,10 @@ import (
 	. "github.com/onsi/gomega"
 	operv1 "github.com/openshift/api/operator/v1"
 	"github.com/openshift/cluster-network-operator/pkg/names"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes/fake"
 )
 
 var MultusAdmissionControllerConfig = operv1.Network{
@@ -36,15 +40,37 @@ func TestRenderMultusAdmissionController(t *testing.T) {
 	config.DisableMultiNetwork = &disabled
 	fillDefaults(config, nil)
 
+	fakeClient := fake.NewSimpleClientset(
+		&corev1.Namespace{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "test1-ignored",
+				Labels: map[string]string{
+					"openshift.io/cluster-monitoring": "true",
+				},
+			},
+		},
+		&corev1.Namespace{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "test2-not-ignored",
+			},
+		},
+		&corev1.Namespace{ObjectMeta: metav1.ObjectMeta{
+			Name: "test3-ignored",
+			Labels: map[string]string{
+				"openshift.io/cluster-monitoring": "true",
+			},
+		},
+		})
+
 	// disable MultusAdmissionController
-	objs, err := renderMultusAdmissionController(config, manifestDir, false)
+	objs, err := renderMultusAdmissionController(config, manifestDir, false, fakeClient)
 	g.Expect(err).NotTo(HaveOccurred())
 	g.Expect(objs).NotTo(ContainElement(HaveKubernetesID("Deployment", "openshift-multus", "multus-admission-controller")))
 
 	// enable MultusAdmissionController
 	enabled := false
 	config.DisableMultiNetwork = &enabled
-	objs, err = renderMultusAdmissionController(config, manifestDir, false)
+	objs, err = renderMultusAdmissionController(config, manifestDir, false, fakeClient)
 	g.Expect(err).NotTo(HaveOccurred())
 	g.Expect(objs).To(ContainElement(HaveKubernetesID("Deployment", "openshift-multus", "multus-admission-controller")))
 
@@ -55,4 +81,34 @@ func TestRenderMultusAdmissionController(t *testing.T) {
 	g.Expect(objs).To(ContainElement(HaveKubernetesID("ClusterRoleBinding", "", "multus-admission-controller-webhook")))
 	g.Expect(objs).To(ContainElement(HaveKubernetesID("ValidatingWebhookConfiguration", "", names.MULTUS_VALIDATING_WEBHOOK)))
 	g.Expect(objs).To(ContainElement(HaveKubernetesID("Deployment", "openshift-multus", "multus-admission-controller")))
+}
+
+// TestRenderMultusAdmissionControllerGetNamespace tests getOpenshiftNamespaces()
+func TestRenderMultusAdmissionControllerGetNamespace(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	fakeClient := fake.NewSimpleClientset(
+		&corev1.Namespace{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "test1-ignored",
+				Labels: map[string]string{
+					"openshift.io/cluster-monitoring": "true",
+				},
+			},
+		},
+		&corev1.Namespace{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "test2-not-ignored",
+			},
+		},
+		&corev1.Namespace{ObjectMeta: metav1.ObjectMeta{
+			Name: "test3-ignored",
+			Labels: map[string]string{
+				"openshift.io/cluster-monitoring": "true",
+			},
+		},
+		})
+	namespaces, err := getOpenshiftNamespaces(fakeClient)
+	g.Expect(err).NotTo(HaveOccurred())
+	g.Expect(namespaces).To(Equal("test1-ignored,test3-ignored"))
 }
