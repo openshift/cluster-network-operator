openshift/cluster-network-operator

commit ee502976611d06df811054d93a11936f54c1c18d

The Multus admission controller should run as a deployment
It's been running as a daemonset that will run on all masters, however, this isn't necessary, especially in large clusters. This causes some additional apiserver traffic when nodes are rebooted as well.

The intent here is to run two instances for redundancy.

bindata/network/multus-admission-controller/admission-controller.yaml
@@ -1,5 +1,5 @@
 ---
-kind: DaemonSet
+kind: Deployment
 apiVersion: apps/v1
 metadata:
   name: multus-admission-controller
@@ -8,16 +8,15 @@ metadata:
     app: multus-admission-controller
   annotations:
     kubernetes.io/description: |
-      This daemon set launches the Multus admisson controller component on each node.
+      This deployment launches the Multus admisson controller component.
     release.openshift.io/version: "{{.ReleaseVersion}}"
     networkoperator.openshift.io/non-critical: ""
 spec:
+  replicas: 2
   selector:
     matchLabels:
       app: multus-admission-controller
       namespace: openshift-multus
-  updateStrategy:
-    type: RollingUpdate
   template:
     metadata:
       annotations:

pkg/network/multus_admission_controller_test.go
@@ -39,14 +39,14 @@ func TestRenderMultusAdmissionController(t *testing.T) {
 	// disable MultusAdmissionController
 	objs, err := renderMultusAdmissionController(config, manifestDir, false)
 	g.Expect(err).NotTo(HaveOccurred())
-	g.Expect(objs).NotTo(ContainElement(HaveKubernetesID("DaemonSet", "openshift-multus", "multus-admission-controller")))
+	g.Expect(objs).NotTo(ContainElement(HaveKubernetesID("Deployment", "openshift-multus", "multus-admission-controller")))
 
 	// enable MultusAdmissionController
 	enabled := false
 	config.DisableMultiNetwork = &enabled
 	objs, err = renderMultusAdmissionController(config, manifestDir, false)
 	g.Expect(err).NotTo(HaveOccurred())
-	g.Expect(objs).To(ContainElement(HaveKubernetesID("DaemonSet", "openshift-multus", "multus-admission-controller")))
+	g.Expect(objs).To(ContainElement(HaveKubernetesID("Deployment", "openshift-multus", "multus-admission-controller")))
 
 	// Check rendered object
 	g.Expect(len(objs)).To(Equal(9))
@@ -54,5 +54,5 @@ func TestRenderMultusAdmissionController(t *testing.T) {
 	g.Expect(objs).To(ContainElement(HaveKubernetesID("ClusterRole", "", "multus-admission-controller-webhook")))
 	g.Expect(objs).To(ContainElement(HaveKubernetesID("ClusterRoleBinding", "", "multus-admission-controller-webhook")))
 	g.Expect(objs).To(ContainElement(HaveKubernetesID("ValidatingWebhookConfiguration", "", names.MULTUS_VALIDATING_WEBHOOK)))
-	g.Expect(objs).To(ContainElement(HaveKubernetesID("DaemonSet", "openshift-multus", "multus-admission-controller")))
+	g.Expect(objs).To(ContainElement(HaveKubernetesID("Deployment", "openshift-multus", "multus-admission-controller")))
 }

commit 3d24e5dfe62c67acd8243f16169ae0d19db5e3ff

Bug 2116982: multus-admission-controller SNO number of replicas
Fixes bug introduced by ee502976611d06df811054d93a11936f54c1c18d

The number of replicas for the multus admission controller should be
determined according to the control-plane or infrastructure topology of
the cluster instead of being hard-coded 1.

This is because on SNO clusters, we want core component deployments to
have a smaller foot-print, at the cost of lowered availability. We have
an e2e test that makes sure all `openshift.*` namespace Deployments and
StatefulSets have 1 replicas when the topologies indicate SNO.

See `Test_getMultusAdmissionControllerReplicas` for exactly how many
replicas are chosen depending on the various combinations of
`ControlPlaneTopology`/`InfrastructureTopology` values.

bindata/network/multus-admission-controller/admission-controller.yaml
@@ -12,7 +12,7 @@ metadata:
     release.openshift.io/version: "{{.ReleaseVersion}}"
     networkoperator.openshift.io/non-critical: ""
 spec:
-  replicas: 2
+  replicas: {{.Replicas}}
   selector:
     matchLabels:
       app: multus-admission-controller

pkg/bootstrap/types.go
@@ -73,10 +73,11 @@ type BootstrapResult struct {
 }
 
 type InfraStatus struct {
-	PlatformType         configv1.PlatformType
-	PlatformRegion       string
-	PlatformStatus       *configv1.PlatformStatus
-	ExternalControlPlane bool
+	PlatformType           configv1.PlatformType
+	PlatformRegion         string
+	PlatformStatus         *configv1.PlatformStatus
+	ControlPlaneTopology   configv1.TopologyMode
+	InfrastructureTopology configv1.TopologyMode
 
 	// KubeCloudConfig is the contents of the openshift-config-managed/kube-cloud-config ConfigMap
 	KubeCloudConfig map[string]string

pkg/controller/operconfig/mtu_probe.go
@@ -39,7 +39,7 @@ const (
 // If, for whatever reason, it takes longer for the MTU to be detected,
 // it will adopt an existing job.
 func (r *ReconcileOperConfig) probeMTU(ctx context.Context, oc *operv1.Network, infra *bootstrap.InfraStatus) (int, error) {
-	if infra.ExternalControlPlane {
+	if infra.ControlPlaneTopology == configv1.ExternalTopologyMode {
 		if infra.PlatformType == configv1.AWSPlatformType {
 			klog.Infof("AWS cluster, omitting MTU probing and using default of %d", awsMTU)
 			return awsMTU, nil

pkg/network/cloud_network.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/pkg/errors"
 
+	configv1 "github.com/openshift/api/config/v1"
 	v1 "github.com/openshift/api/config/v1"
 	operv1 "github.com/openshift/api/operator/v1"
 	"github.com/openshift/cluster-network-operator/pkg/bootstrap"
@@ -39,7 +40,7 @@ func renderCloudNetworkConfigController(conf *operv1.NetworkSpec, cloudBootstrap
 	data.Data["CloudNetworkConfigControllerImage"] = os.Getenv("CLOUD_NETWORK_CONFIG_CONTROLLER_IMAGE")
 	data.Data["KubernetesServiceHost"] = cloudBootstrapResult.APIServers[bootstrap.APIServerDefault].Host
 	data.Data["KubernetesServicePort"] = cloudBootstrapResult.APIServers[bootstrap.APIServerDefault].Port
-	data.Data["ExternalControlPlane"] = cloudBootstrapResult.ExternalControlPlane
+	data.Data["ExternalControlPlane"] = cloudBootstrapResult.ControlPlaneTopology == configv1.ExternalTopologyMode
 	data.Data["PlatformAzureEnvironment"] = ""
 	data.Data["PlatformAWSCAPath"] = ""
 	data.Data["HTTP_PROXY"] = cloudBootstrapResult.Proxy.HTTPProxy

pkg/network/kuryr.go
@@ -11,6 +11,7 @@ import (
 
 	uns "k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 
+	configv1 "github.com/openshift/api/config/v1"
 	operv1 "github.com/openshift/api/operator/v1"
 	"github.com/openshift/cluster-network-operator/pkg/bootstrap"
 	"github.com/openshift/cluster-network-operator/pkg/render"
@@ -44,7 +45,7 @@ func renderKuryr(conf *operv1.NetworkSpec, bootstrapResult *bootstrap.BootstrapR
 	data.Data["UserCACertificate"] = b.UserCACert
 
 	// ExternalControlPlane
-	data.Data["ExternalControlPlane"] = bootstrapResult.Infra.ExternalControlPlane
+	data.Data["ExternalControlPlane"] = bootstrapResult.Infra.ControlPlaneTopology == configv1.ExternalTopologyMode
 
 	data.Data["HttpsProxy"] = b.HttpsProxy
 	data.Data["HttpProxy"] = b.HttpProxy

pkg/network/multus_admission_controller.go
@@ -11,7 +11,7 @@ import (
 )
 
 // renderMultusAdmissonControllerConfig returns the manifests of Multus Admisson Controller
-func renderMultusAdmissonControllerConfig(manifestDir string, externalControlPlane bool) ([]*uns.Unstructured, error) {
+func renderMultusAdmissonControllerConfig(manifestDir string, externalControlPlane bool, replicas int) ([]*uns.Unstructured, error) {
 	objs := []*uns.Unstructured{}
 
 	// render the manifests on disk
@@ -21,6 +21,7 @@ func renderMultusAdmissonControllerConfig(manifestDir string, externalControlPla
 	data.Data["MultusValidatingWebhookName"] = names.MULTUS_VALIDATING_WEBHOOK
 	data.Data["KubeRBACProxyImage"] = os.Getenv("KUBE_RBAC_PROXY_IMAGE")
 	data.Data["ExternalControlPlane"] = externalControlPlane
+	data.Data["Replicas"] = replicas
 
 	manifests, err := render.RenderDir(filepath.Join(manifestDir, "network/multus-admission-controller"), &data)
 	if err != nil {

pkg/network/openshift_sdn.go
@@ -53,7 +53,7 @@ func renderOpenShiftSDN(conf *operv1.NetworkSpec, bootstrapResult *bootstrap.Boo
 	} else {
 		data.Data["SDNPlatformAzure"] = false
 	}
-	data.Data["ExternalControlPlane"] = bootstrapResult.Infra.ExternalControlPlane
+	data.Data["ExternalControlPlane"] = bootstrapResult.Infra.ControlPlaneTopology == configv1.ExternalTopologyMode
 	data.Data["RoutableMTU"] = nil
 	data.Data["MTU"] = nil
 

pkg/network/ovn_kubernetes.go
@@ -80,7 +80,8 @@ func renderOVNKubernetes(conf *operv1.NetworkSpec, bootstrapResult *bootstrap.Bo
 
 	// TODO: Fix operator behavior when running in a cluster with an externalized control plane.
 	// For now, return an error since we don't have any master nodes to run the ovn-master daemonset.
-	if bootstrapResult.Infra.ExternalControlPlane && !bootstrapResult.OVN.OVNKubernetesConfig.HyperShiftConfig.Enabled {
+	externalControlPlane := bootstrapResult.Infra.ControlPlaneTopology == configv1.ExternalTopologyMode
+	if externalControlPlane && !bootstrapResult.OVN.OVNKubernetesConfig.HyperShiftConfig.Enabled {
 		return nil, progressing, fmt.Errorf("Unable to render OVN in a cluster with an external control plane")
 	}
 

pkg/network/render.go
@@ -42,7 +42,8 @@ func Render(conf *operv1.NetworkSpec, bootstrapResult *bootstrap.BootstrapResult
 	objs = append(objs, o...)
 
 	// render MultusAdmissionController
-	o, err = renderMultusAdmissionController(conf, manifestDir, bootstrapResult.Infra.ExternalControlPlane)
+	o, err = renderMultusAdmissionController(conf, manifestDir,
+		bootstrapResult.Infra.ControlPlaneTopology == configv1.ExternalTopologyMode, bootstrapResult)
 	if err != nil {
 		return nil, progressing, err
 	}
@@ -600,8 +601,21 @@ func renderAdditionalNetworks(conf *operv1.NetworkSpec, manifestDir string) ([]*
 	return out, nil
 }
 
+func getMultusAdmissionControllerReplicas(bootstrapResult *bootstrap.BootstrapResult) int {
+	replicas := 2
+	if bootstrapResult.Infra.ControlPlaneTopology == configv1.ExternalTopologyMode {
+		if bootstrapResult.Infra.InfrastructureTopology == configv1.SingleReplicaTopologyMode {
+			replicas = 1
+		}
+	} else if bootstrapResult.Infra.ControlPlaneTopology == configv1.SingleReplicaTopologyMode {
+		replicas = 1
+	}
+
+	return replicas
+}
+
 // renderMultusAdmissionController generates the manifests of Multus Admission Controller
-func renderMultusAdmissionController(conf *operv1.NetworkSpec, manifestDir string, externalControlPlane bool) ([]*uns.Unstructured, error) {
+func renderMultusAdmissionController(conf *operv1.NetworkSpec, manifestDir string, externalControlPlane bool, bootstrapResult *bootstrap.BootstrapResult) ([]*uns.Unstructured, error) {
 	if *conf.DisableMultiNetwork {
 		return nil, nil
 	}
@@ -609,7 +623,8 @@ func renderMultusAdmissionController(conf *operv1.NetworkSpec, manifestDir strin
 	var err error
 	out := []*uns.Unstructured{}
 
-	objs, err := renderMultusAdmissonControllerConfig(manifestDir, externalControlPlane)
+	objs, err := renderMultusAdmissonControllerConfig(manifestDir, externalControlPlane,
+		getMultusAdmissionControllerReplicas(bootstrapResult))
 	if err != nil {
 		return nil, err
 	}

pkg/platform/platform.go
@@ -27,15 +27,16 @@ func InfraStatus(client cnoclient.Client) (*bootstrap.InfraStatus, error) {
 	}
 
 	res := &bootstrap.InfraStatus{
-		PlatformType:         infraConfig.Status.PlatformStatus.Type,
-		PlatformStatus:       infraConfig.Status.PlatformStatus,
-		ExternalControlPlane: infraConfig.Status.ControlPlaneTopology == configv1.ExternalTopologyMode,
-		APIServers:           map[string]bootstrap.APIServer{},
+		PlatformType:           infraConfig.Status.PlatformStatus.Type,
+		PlatformStatus:         infraConfig.Status.PlatformStatus,
+		ControlPlaneTopology:   infraConfig.Status.ControlPlaneTopology,
+		InfrastructureTopology: infraConfig.Status.InfrastructureTopology,
+		APIServers:             map[string]bootstrap.APIServer{},
 	}
 
 	// If we use hypershift, the components need proxy settings. In selfhosted, they
 	// are always local to the API so there it is never needed.
-	if res.ExternalControlPlane {
+	if res.ControlPlaneTopology == configv1.ExternalTopologyMode {
 		proxy := &configv1.Proxy{}
 		if err := client.Default().CRClient().Get(context.TODO(), types.NamespacedName{Name: "cluster"}, proxy); err != nil {
 			return nil, fmt.Errorf("failed to get proxy 'cluster': %w", err)

pkg/controller/operconfig/mtu_probe_test.go
@@ -24,7 +24,7 @@ func TestProbeMTU(t *testing.T) {
 	}{
 		{
 			name:        "AWS on Hypershift, hardcoded value is used",
-			infra:       &bootstrap.InfraStatus{PlatformType: configv1.AWSPlatformType, ExternalControlPlane: true},
+			infra:       &bootstrap.InfraStatus{PlatformType: configv1.AWSPlatformType, ControlPlaneTopology: configv1.ExternalTopologyMode},
 			expectedMTU: 9001,
 		},
 		{
@@ -38,7 +38,7 @@ func TestProbeMTU(t *testing.T) {
 		},
 		{
 			name:        "Azure on hypershift, hardcoded value is used",
-			infra:       &bootstrap.InfraStatus{PlatformType: configv1.AzurePlatformType, ExternalControlPlane: true},
+			infra:       &bootstrap.InfraStatus{PlatformType: configv1.AzurePlatformType, ControlPlaneTopology: configv1.ExternalTopologyMode},
 			expectedMTU: 1500,
 		},
 		{
@@ -52,7 +52,7 @@ func TestProbeMTU(t *testing.T) {
 		},
 		{
 			name:  "Unknown platform on Hypershift, value from configmap is used",
-			infra: &bootstrap.InfraStatus{ExternalControlPlane: true},
+			infra: &bootstrap.InfraStatus{ControlPlaneTopology: configv1.ExternalTopologyMode},
 			objects: []crclient.Object{&corev1.ConfigMap{
 				ObjectMeta: metav1.ObjectMeta{Namespace: cmNamespace, Name: cmName},
 				Data:       map[string]string{"mtu": "5000"},

pkg/network/multus_admission_controller_test.go
@@ -36,15 +36,17 @@ func TestRenderMultusAdmissionController(t *testing.T) {
 	config.DisableMultiNetwork = &disabled
 	fillDefaults(config, nil)
 
+	bootstrap := fakeBootstrapResult()
+
 	// disable MultusAdmissionController
-	objs, err := renderMultusAdmissionController(config, manifestDir, false)
+	objs, err := renderMultusAdmissionController(config, manifestDir, false, bootstrap)
 	g.Expect(err).NotTo(HaveOccurred())
 	g.Expect(objs).NotTo(ContainElement(HaveKubernetesID("Deployment", "openshift-multus", "multus-admission-controller")))
 
 	// enable MultusAdmissionController
 	enabled := false
 	config.DisableMultiNetwork = &enabled
-	objs, err = renderMultusAdmissionController(config, manifestDir, false)
+	objs, err = renderMultusAdmissionController(config, manifestDir, false, bootstrap)
 	g.Expect(err).NotTo(HaveOccurred())
 	g.Expect(objs).To(ContainElement(HaveKubernetesID("Deployment", "openshift-multus", "multus-admission-controller")))
 

pkg/network/render_test.go
@@ -4,12 +4,12 @@ import (
 	"testing"
 
 	. "github.com/onsi/gomega"
+	configv1 "github.com/openshift/api/config/v1"
+	operv1 "github.com/openshift/api/operator/v1"
+	"github.com/openshift/cluster-network-operator/pkg/bootstrap"
 	"github.com/openshift/cluster-network-operator/pkg/client/fake"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes/scheme"
-
-	configv1 "github.com/openshift/api/config/v1"
-	operv1 "github.com/openshift/api/operator/v1"
 )
 
 func TestIsChangeSafe(t *testing.T) {
@@ -283,4 +283,95 @@ func TestRenderUnknownNetwork(t *testing.T) {
 
 func fillDefaults(conf, previous *operv1.NetworkSpec) {
 	FillDefaults(conf, previous, 1400)
+}
+
+func Test_getMultusAdmissionControllerReplicas(t *testing.T) {
+	type args struct {
+		bootstrapResult *bootstrap.BootstrapResult
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "External control plane, highly available infra",
+			args: args{
+				bootstrapResult: &bootstrap.BootstrapResult{
+					Infra: bootstrap.InfraStatus{
+						ControlPlaneTopology:   configv1.ExternalTopologyMode,
+						InfrastructureTopology: configv1.HighlyAvailableTopologyMode,
+					},
+				},
+			},
+			want: 2,
+		},
+		{
+			name: "External control plane, single-replica infra",
+			args: args{
+				bootstrapResult: &bootstrap.BootstrapResult{
+					Infra: bootstrap.InfraStatus{
+						ControlPlaneTopology:   configv1.ExternalTopologyMode,
+						InfrastructureTopology: configv1.SingleReplicaTopologyMode,
+					},
+				},
+			},
+			want: 1,
+		},
+		{
+			name: "Highly available control-plane, highly available infra",
+			args: args{
+				bootstrapResult: &bootstrap.BootstrapResult{
+					Infra: bootstrap.InfraStatus{
+						ControlPlaneTopology:   configv1.HighlyAvailableTopologyMode,
+						InfrastructureTopology: configv1.HighlyAvailableTopologyMode,
+					},
+				},
+			},
+			want: 2,
+		},
+		{
+			name: "Highly available control-plane, single-replica infra",
+			args: args{
+				bootstrapResult: &bootstrap.BootstrapResult{
+					Infra: bootstrap.InfraStatus{
+						ControlPlaneTopology:   configv1.HighlyAvailableTopologyMode,
+						InfrastructureTopology: configv1.SingleReplicaTopologyMode,
+					},
+				},
+			},
+			want: 2,
+		},
+		{
+			name: "Single-replicas control-plane, single-replica infra",
+			args: args{
+				bootstrapResult: &bootstrap.BootstrapResult{
+					Infra: bootstrap.InfraStatus{
+						ControlPlaneTopology:   configv1.SingleReplicaTopologyMode,
+						InfrastructureTopology: configv1.SingleReplicaTopologyMode,
+					},
+				},
+			},
+			want: 1,
+		},
+		{
+			name: "Single-replicas control-plane, highly-available infra",
+			args: args{
+				bootstrapResult: &bootstrap.BootstrapResult{
+					Infra: bootstrap.InfraStatus{
+						ControlPlaneTopology:   configv1.SingleReplicaTopologyMode,
+						InfrastructureTopology: configv1.HighlyAvailableTopologyMode,
+					},
+				},
+			},
+			want: 1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := getMultusAdmissionControllerReplicas(tt.args.bootstrapResult); got != tt.want {
+				t.Errorf("getMultusAdmissionControllerReplicas() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }

pkg/network/testutil_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/onsi/gomega/types"
+	configv1 "github.com/openshift/api/config/v1"
 	"github.com/openshift/cluster-network-operator/pkg/bootstrap"
 	uns "k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 )
@@ -59,9 +60,10 @@ func (k *KubeObjectMatcher) NegatedFailureMessage(actual interface{}) string {
 func fakeBootstrapResult() *bootstrap.BootstrapResult {
 	return &bootstrap.BootstrapResult{
 		Infra: bootstrap.InfraStatus{
-			PlatformType:         "GCP",
-			PlatformRegion:       "moon-2",
-			ExternalControlPlane: false,
+			PlatformType:           "GCP",
+			PlatformRegion:         "moon-2",
+			ControlPlaneTopology:   configv1.HighlyAvailableTopologyMode,
+			InfrastructureTopology: configv1.HighlyAvailableTopologyMode,
 			APIServers: map[string]bootstrap.APIServer{
 				bootstrap.APIServerDefault: {
 					Host: "testing.test",

pkg/platform/platform_test.go
@@ -1,60 +1,0 @@
-package platform
-
-import (
-	"testing"
-
-	configv1 "github.com/openshift/api/config/v1"
-	"github.com/openshift/cluster-network-operator/pkg/client/fake"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/kubernetes/scheme"
-)
-
-func TestTopologyModeDetection(t *testing.T) {
-	testCases := []struct {
-		name                       string
-		infrastructure             *configv1.Infrastructure
-		expectExternalControlplane bool
-	}{
-		{
-			name: "External controlplane toplogy",
-			infrastructure: &configv1.Infrastructure{
-				ObjectMeta: metav1.ObjectMeta{Name: "cluster"},
-				Status: configv1.InfrastructureStatus{
-					PlatformStatus:       &configv1.PlatformStatus{},
-					ControlPlaneTopology: configv1.ExternalTopologyMode,
-				},
-			},
-			expectExternalControlplane: true,
-		},
-		{
-			name: "Not expectExternalControlplane",
-			infrastructure: &configv1.Infrastructure{
-				ObjectMeta: metav1.ObjectMeta{Name: "cluster"},
-				Status: configv1.InfrastructureStatus{
-					PlatformStatus:       &configv1.PlatformStatus{},
-					ControlPlaneTopology: configv1.HighlyAvailableTopologyMode,
-				},
-			},
-			expectExternalControlplane: false,
-		},
-	}
-
-	if err := configv1.AddToScheme(scheme.Scheme); err != nil {
-		t.Fatalf("failed to add configv1 to scheme: %v", err)
-	}
-
-	for _, tc := range testCases {
-		t.Run(tc.name, func(t *testing.T) {
-			client := fake.NewFakeClient(tc.infrastructure, &configv1.Proxy{ObjectMeta: metav1.ObjectMeta{Name: "cluster"}})
-
-			bootstrapResult, err := InfraStatus(client)
-			if err != nil {
-				t.Fatalf("BootstrapInfra failed: %v", err)
-			}
-
-			if bootstrapResult.ExternalControlPlane != tc.expectExternalControlplane {
-				t.Errorf("expected externalControlPlane to be %t, was %t", tc.expectExternalControlplane, bootstrapResult.ExternalControlPlane)
-			}
-		})
-	}
-}