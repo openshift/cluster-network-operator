b5d4493f1b6035aa81ca4fde5b8e823e4741a615
nicklesimba <simha.nikhil@gmail.com>
2022-09-13 22:26:45 -0400

    Added unit tests for egressIP migration and rollback
    - Added unit tests as described
    - Cleaned up unecessary or redundant information from multicast tests
    
    Signed-off-by: nicklesimba <simha.nikhil@gmail.com>

--- pkg/controller/operconfig/migration_test.go
+++ pkg/controller/operconfig/migration_test.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"testing"
 
-	// . "github.com/onsi/gomega"
+	. "github.com/onsi/gomega"
 	v1 "github.com/openshift/api/network/v1"
 	"github.com/openshift/cluster-network-operator/pkg/client/fake"
 	corev1 "k8s.io/api/core/v1"
@@ -17,9 +17,470 @@ import (
 )
 
 const testMigrationNamespace = "openshift-multus"
+const testMigrationHost = "egressip-test-host"
 
 func init() {
 	utilruntime.Must(v1.AddToScheme(scheme.Scheme))
+}
+
+func TestEgressIpMigration(t *testing.T) {
+	nnsEgressIpsStrArrSingle := []string{"10.0.128.5"}
+	nnsEgressIpsIfcArrSingle := ConvertToUnstructuredInterface(nnsEgressIpsStrArrSingle)
+	nnsEgressIpsStrArrMult := []string{"10.0.128.5", "10.0.128.6", "10.0.128.7"}
+	nnsEgressIpsIfcArrMult := ConvertToUnstructuredInterface(nnsEgressIpsStrArrMult)
+
+	testCases := []struct {
+		name                 string
+		objects              []crclient.Object
+		expectedEgressIpList []string
+	}{
+		{
+			name: "Hostsubnet has automatic config and netnamespace has one egressIP",
+			objects: []crclient.Object{
+				&v1.HostSubnet{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: testMigrationHost,
+					},
+					EgressCIDRs: []v1.HostSubnetEgressCIDR{"10.0.128.0/17"},
+					EgressIPs:   []v1.HostSubnetEgressIP{"10.0.128.5"},
+					Host:        testMigrationHost,
+				},
+				&uns.Unstructured{
+					Object: map[string]interface{}{
+						"apiVersion": "network.openshift.io/v1",
+						"kind":       "NetNamespace",
+						"egressIPs":  nnsEgressIpsIfcArrSingle,
+						"netname":    testMigrationNamespace,
+						"metadata": map[string]interface{}{
+							"name": testMigrationNamespace,
+						},
+					},
+				},
+				&corev1.Node{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: testMigrationHost,
+					},
+				},
+				&corev1.Namespace{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: testMigrationNamespace,
+					},
+				},
+			},
+			expectedEgressIpList: nnsEgressIpsStrArrSingle,
+		},
+		{
+			name: "Hostsubnet has automatic config and netnamespace has multiple egressIPs",
+			objects: []crclient.Object{
+				&v1.HostSubnet{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: testMigrationHost,
+					},
+					EgressCIDRs: []v1.HostSubnetEgressCIDR{"10.0.128.0/17"},
+					EgressIPs:   []v1.HostSubnetEgressIP{"10.0.128.5", "10.0.128.6", "10.0.128.7"},
+					Host:        testMigrationHost,
+				},
+				&uns.Unstructured{
+					Object: map[string]interface{}{
+						"apiVersion": "network.openshift.io/v1",
+						"kind":       "NetNamespace",
+						"egressIPs":  nnsEgressIpsIfcArrMult,
+						"netname":    testMigrationNamespace,
+						"metadata": map[string]interface{}{
+							"name": testMigrationNamespace,
+						},
+					},
+				},
+				&corev1.Node{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: testMigrationHost,
+					},
+				},
+				&corev1.Namespace{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: testMigrationNamespace,
+					},
+				},
+			},
+			expectedEgressIpList: nnsEgressIpsStrArrMult,
+		},
+		{
+			name: "Hostsubnet has manual config and netnamespace has one egressIP",
+			objects: []crclient.Object{
+				&v1.HostSubnet{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: testMigrationHost,
+					},
+					EgressIPs: []v1.HostSubnetEgressIP{"10.0.128.5"},
+					Host:      testMigrationHost,
+				},
+				&uns.Unstructured{
+					Object: map[string]interface{}{
+						"apiVersion": "network.openshift.io/v1",
+						"kind":       "NetNamespace",
+						"egressIPs":  nnsEgressIpsIfcArrSingle,
+						"netname":    testMigrationNamespace,
+						"metadata": map[string]interface{}{
+							"name": testMigrationNamespace,
+						},
+					},
+				},
+				&corev1.Node{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: testMigrationHost,
+					},
+				},
+				&corev1.Namespace{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: testMigrationNamespace,
+					},
+				},
+			},
+			expectedEgressIpList: nnsEgressIpsStrArrSingle,
+		},
+		{
+			name: "Hostsubnet has manual config and netnamespace has multiple egressIPs",
+			objects: []crclient.Object{
+				&v1.HostSubnet{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: testMigrationHost,
+					},
+					EgressIPs: []v1.HostSubnetEgressIP{"10.0.128.5", "10.0.128.6", "10.0.128.7"},
+					Host:      testMigrationHost,
+				},
+				&uns.Unstructured{
+					Object: map[string]interface{}{
+						"apiVersion": "network.openshift.io/v1",
+						"kind":       "NetNamespace",
+						"egressIPs":  nnsEgressIpsIfcArrMult,
+						"netname":    testMigrationNamespace,
+						"metadata": map[string]interface{}{
+							"name": testMigrationNamespace,
+						},
+					},
+				},
+				&corev1.Node{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: testMigrationHost,
+					},
+				},
+				&corev1.Namespace{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: testMigrationNamespace,
+					},
+				},
+			},
+			expectedEgressIpList: nnsEgressIpsStrArrMult,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			g := NewWithT(t)
+			client := fake.NewFakeClient(tc.objects...)
+
+			egressIpList, _, err := convertSdnEgressIpToOvnEgressIp(context.Background(), client)
+			if err != nil {
+				t.Fatalf("convertSdnEgressIpToOvnEgressIp: %v", err)
+			}
+			// Collect all egressIP strings in a single slice
+			var egressIpValueList []string
+			for _, egressIp := range egressIpList {
+				egressIpsSlice := egressIp.Object["spec"].(map[string]interface{})["egressIPs"].([]interface{})
+				egressIpsStringSlice := make([]string, len(egressIpsSlice))
+				for i := range egressIpsSlice {
+					egressIpsStringSlice[i] = egressIpsSlice[i].(string)
+				}
+				egressIpValueList = append(egressIpValueList, egressIpsStringSlice...)
+			}
+
+			// Check if values match exactly
+			expectedEgressIpList := ConvertToUnstructuredInterface(tc.expectedEgressIpList)
+			g.Expect(egressIpValueList).To(ConsistOf(expectedEgressIpList...), "expected applied OVN egressIPs to have egressIP list matching input SDN config")
+
+			// Check that node annotation has been added
+			nodeObj, err := client.Default().Kubernetes().CoreV1().Nodes().Get(context.Background(), testMigrationHost, metav1.GetOptions{})
+			if err != nil {
+				t.Errorf("unexpected error: %v", err)
+			}
+			if _, ok := nodeObj.Labels[egressAssignable]; !ok {
+				t.Errorf("expect node to be marked with egress-assignable annotation")
+			}
+		})
+	}
+}
+
+func TestEgressIpRollbackMigration(t *testing.T) {
+	egressIpsStrArrSingle := []string{"10.0.128.5"}
+	egressIpsIfcArrSingle := ConvertToUnstructuredInterface(egressIpsStrArrSingle)
+	egressIpsStrArrMult := []string{"10.0.128.5", "10.0.128.6", "10.0.128.7"}
+	egressIpsIfcArrMult := ConvertToUnstructuredInterface(egressIpsStrArrMult)
+
+	egressCIDRsStrArr := []string{"10.0.128.0/17"}
+	nodeAnnotationMap := make(map[string]string, 0)
+	nodeAnnotationMap[egressIPNodeConfig] = "[{\"interface\":\"nic0\",\"ifaddr\":{\"ipv4\":\"10.0.128.0/17\"},\"capacity\":{\"ip\":10}}]"
+
+	egressCIDRsStrArrWithEgressCidrAnnotation := []string{"10.0.128.0/18"}
+	nodeAnnotationMapWithEgressCidrAnnotation := make(map[string]string, 0)
+	nodeAnnotationMapWithEgressCidrAnnotation[egressIPNodeConfig] = "[{\"interface\":\"nic0\",\"ifaddr\":{\"ipv4\":\"10.0.128.0/17\"},\"capacity\":{\"ip\":10}}]"
+	nodeAnnotationMapWithEgressCidrAnnotation[egressCIDRAnnotationName] = "{\"EgressCIDRs\":[\"10.0.128.0/18\"]}"
+
+	nodeLabelMap := make(map[string]string)
+	nodeLabelMap[egressAssignable] = ""
+
+	testCases := []struct {
+		name                    string
+		objects                 []crclient.Object
+		expectedEgressIpsList   []string
+		expectedEgressCIDRsList []string
+	}{
+		{
+			name: "egressIP has one IP listed",
+			objects: []crclient.Object{
+				&v1.HostSubnet{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: testMigrationHost,
+					},
+					Host: testMigrationHost,
+				},
+				&uns.Unstructured{
+					Object: map[string]interface{}{
+						"apiVersion": "network.openshift.io/v1",
+						"kind":       "NetNamespace",
+						"netname":    testMigrationNamespace,
+						"metadata": map[string]interface{}{
+							"name": testMigrationNamespace,
+						},
+					},
+				},
+				&corev1.Node{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:        testMigrationHost,
+						Annotations: nodeAnnotationMap,
+						Labels:      nodeLabelMap,
+					},
+				},
+				&corev1.Namespace{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: testMigrationNamespace,
+					},
+				},
+				&uns.Unstructured{
+					Object: map[string]interface{}{
+						"apiVersion": "k8s.ovn.org/v1",
+						"kind":       "EgressIP",
+						"metadata": map[string]interface{}{
+							"name": "egress-group1",
+						},
+						"spec": map[string]interface{}{
+							"egressIPs": egressIpsIfcArrSingle,
+							"namespaceSelector": map[string]interface{}{
+								"matchLabels": map[string]interface{}{
+									"kubernetes.io/metadata.name": testMigrationNamespace,
+								},
+							},
+						},
+					},
+				},
+			},
+			expectedEgressIpsList:   egressIpsStrArrSingle,
+			expectedEgressCIDRsList: egressCIDRsStrArr,
+		},
+		{
+			name: "egressIP has multiple IPs listed",
+			objects: []crclient.Object{
+				&v1.HostSubnet{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: testMigrationHost,
+					},
+					Host: testMigrationHost,
+				},
+				&uns.Unstructured{
+					Object: map[string]interface{}{
+						"apiVersion": "network.openshift.io/v1",
+						"kind":       "NetNamespace",
+						"netname":    testMigrationNamespace,
+						"metadata": map[string]interface{}{
+							"name": testMigrationNamespace,
+						},
+					},
+				},
+				&corev1.Node{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:        testMigrationHost,
+						Annotations: nodeAnnotationMap,
+						Labels:      nodeLabelMap,
+					},
+				},
+				&corev1.Namespace{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: testMigrationNamespace,
+					},
+				},
+				&uns.Unstructured{
+					Object: map[string]interface{}{
+						"apiVersion": "k8s.ovn.org/v1",
+						"kind":       "EgressIP",
+						"metadata": map[string]interface{}{
+							"name": "egress-group1",
+						},
+						"spec": map[string]interface{}{
+							"egressIPs": egressIpsIfcArrMult,
+							"namespaceSelector": map[string]interface{}{
+								"matchLabels": map[string]interface{}{
+									"kubernetes.io/metadata.name": testMigrationNamespace,
+								},
+							},
+						},
+					},
+				},
+			},
+			expectedEgressIpsList:   egressIpsStrArrMult,
+			expectedEgressCIDRsList: egressCIDRsStrArr,
+		},
+		{
+			name: "egressIP has one IP listed and node has egressCIDR rollback annotation",
+			objects: []crclient.Object{
+				&v1.HostSubnet{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: testMigrationHost,
+					},
+					Host: testMigrationHost,
+				},
+				&uns.Unstructured{
+					Object: map[string]interface{}{
+						"apiVersion": "network.openshift.io/v1",
+						"kind":       "NetNamespace",
+						"netname":    testMigrationNamespace,
+						"metadata": map[string]interface{}{
+							"name": testMigrationNamespace,
+						},
+					},
+				},
+				&corev1.Node{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:        testMigrationHost,
+						Annotations: nodeAnnotationMapWithEgressCidrAnnotation,
+						Labels:      nodeLabelMap,
+					},
+				},
+				&corev1.Namespace{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: testMigrationNamespace,
+					},
+				},
+				&uns.Unstructured{
+					Object: map[string]interface{}{
+						"apiVersion": "k8s.ovn.org/v1",
+						"kind":       "EgressIP",
+						"metadata": map[string]interface{}{
+							"name": "egress-group1",
+						},
+						"spec": map[string]interface{}{
+							"egressIPs": egressIpsIfcArrSingle,
+							"namespaceSelector": map[string]interface{}{
+								"matchLabels": map[string]interface{}{
+									"kubernetes.io/metadata.name": testMigrationNamespace,
+								},
+							},
+						},
+					},
+				},
+			},
+			expectedEgressIpsList:   egressIpsStrArrSingle,
+			expectedEgressCIDRsList: egressCIDRsStrArrWithEgressCidrAnnotation,
+		},
+		{
+			name: "egressIP has multiple IPs listed and node has egressCIDR rollback annotation",
+			objects: []crclient.Object{
+				&v1.HostSubnet{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: testMigrationHost,
+					},
+					Host: testMigrationHost,
+				},
+				&uns.Unstructured{
+					Object: map[string]interface{}{
+						"apiVersion": "network.openshift.io/v1",
+						"kind":       "NetNamespace",
+						"netname":    testMigrationNamespace,
+						"metadata": map[string]interface{}{
+							"name": testMigrationNamespace,
+						},
+					},
+				},
+				&corev1.Node{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:        testMigrationHost,
+						Annotations: nodeAnnotationMapWithEgressCidrAnnotation,
+						Labels:      nodeLabelMap,
+					},
+				},
+				&corev1.Namespace{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: testMigrationNamespace,
+					},
+				},
+				&uns.Unstructured{
+					Object: map[string]interface{}{
+						"apiVersion": "k8s.ovn.org/v1",
+						"kind":       "EgressIP",
+						"metadata": map[string]interface{}{
+							"name": "egress-group1",
+						},
+						"spec": map[string]interface{}{
+							"egressIPs": egressIpsIfcArrMult,
+							"namespaceSelector": map[string]interface{}{
+								"matchLabels": map[string]interface{}{
+									"kubernetes.io/metadata.name": testMigrationNamespace,
+								},
+							},
+						},
+					},
+				},
+			},
+			expectedEgressIpsList:   egressIpsStrArrMult,
+			expectedEgressCIDRsList: egressCIDRsStrArrWithEgressCidrAnnotation,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			g := NewWithT(t)
+			client := fake.NewFakeClient(tc.objects...)
+
+			err := convertOvnEgressIpToSdnEgressIp(context.Background(), client)
+			if err != nil {
+				t.Fatalf("convertOvnEgressIpToSdnEgressIp: %v", err)
+			}
+
+			// Get the Hostsubnet and Netnamespace that should be updated
+			hsn, err := client.Default().Dynamic().Resource(gvrHostSubnet).Get(context.Background(), testMigrationHost, metav1.GetOptions{})
+			if err != nil {
+				t.Fatalf("failed to get hostsubnet: %v", err)
+			}
+			nns, err := client.Default().Dynamic().Resource(gvrNetnamespace).Get(context.Background(), testMigrationNamespace, metav1.GetOptions{})
+			if err != nil {
+				t.Fatalf("failed to get netnamespace: %v", err)
+			}
+
+			// Check if Hostsubnets has correct egressCIDRs field
+			hsnEgressCIDRs, found, err := uns.NestedStringSlice(hsn.Object, "egressCIDRs")
+			if !found || err != nil {
+				t.Fatalf("failed to find egressCIDRs for hostsubnet %s, probable cause: %v", hsn.GetName(), err)
+			}
+			expectedEgressCIDRsList := ConvertToUnstructuredInterface(tc.expectedEgressCIDRsList)
+			g.Expect(hsnEgressCIDRs).To(ConsistOf(expectedEgressCIDRsList...), "expected applied SDN hostsubnet to have egressCIDRs matching input OVN config")
+
+			// Check if Netnamespaces has correct egressIPs field
+			nnsEgressIPs, found, err := uns.NestedStringSlice(nns.Object, "egressIPs")
+			if !found || err != nil {
+				t.Fatalf("failed to find egressIPs for netnamespace %s, probable cause: %v", nns.GetName(), err)
+			}
+			expectedEgressIpsList := ConvertToUnstructuredInterface(tc.expectedEgressIpsList)
+			g.Expect(nnsEgressIPs).To(ConsistOf(expectedEgressIpsList...), "expected applied SDN netnamespace to have egressIPs matching input OVN config")
+		})
+	}
 }
 
 func TestMulticastMigration(t *testing.T) {
@@ -55,7 +516,6 @@ func TestMulticastMigration(t *testing.T) {
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			// g := gomega.NewWithT(t)
 			client := fake.NewFakeClient(tc.objects...)
 
 			err := enableMulticastOVN(context.Background(), client)
@@ -111,7 +571,6 @@ func TestMulticastMigrationRollback(t *testing.T) {
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			// g := gomega.NewWithT(t)
 			client := fake.NewFakeClient(tc.objects...)
 
 			err := enableMulticastSDN(context.Background(), client)
@@ -133,4 +592,12 @@ func TestMulticastMigrationRollback(t *testing.T) {
 			}
 		})
 	}
+}
+
+func ConvertToUnstructuredInterface(input []string) []interface{} {
+	output := make([]interface{}, len(input))
+	for i, v := range input {
+		output[i] = v
+	}
+	return output
 }
