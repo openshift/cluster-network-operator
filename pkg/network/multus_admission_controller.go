42f3707b8f3d2b85c1277186dbe61d46f5f2f2dd
Martin Kennelly <mkennell@redhat.com>
2023-04-03 15:16:35 +0100

    Migrate to hypershift v1beta1
    Signed-off-by: Martin Kennelly <mkennell@redhat.com>

--- pkg/network/multus_admission_controller.go
+++ pkg/network/multus_admission_controller.go
@@ -8,7 +8,7 @@ import (
 	"path/filepath"
 	"strings"
 
-	hyperv1 "github.com/openshift/hypershift/api/v1alpha1"
+	hyperv1 "github.com/openshift/hypershift/api/v1beta1"
 
 	"github.com/openshift/cluster-network-operator/pkg/bootstrap"
 	cnoclient "github.com/openshift/cluster-network-operator/pkg/client"
