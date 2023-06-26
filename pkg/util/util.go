a5dc8b5d98121f4dfa47f8957b0c7ad44fad8575
Riccardo Ravaioli <rravaiol@redhat.com>
2023-06-26 18:36:21 +0200

    Don't update CNO version until 2-phase upgrade is over
    Signed-off-by: Riccardo Ravaioli <rravaiol@redhat.com>

---
+++ pkg/util/util.go
@@ -0,0 +1,16 @@
+package util
+
+import (
+	"context"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+)
+
+const OVN_INTERCONNECT_CONFIGMAP_NAME = "ovn-interconnect-configuration"
+const OVN_NAMESPACE = "openshift-ovn-kubernetes"
+
+func GetInterConnectConfigMap(kubeClient kubernetes.Interface) (*corev1.ConfigMap, error) {
+	return kubeClient.CoreV1().ConfigMaps(OVN_NAMESPACE).Get(context.TODO(), OVN_INTERCONNECT_CONFIGMAP_NAME, metav1.GetOptions{})
+}
