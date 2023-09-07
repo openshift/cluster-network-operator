82cb8431bda869f9bb59c5ef49c1f775300ac417
Dan Williams <dcbw@redhat.com>
2023-09-07 15:11:00 -0500

    onethread

--- pkg/network/ovn_kubernetes.go
+++ pkg/network/ovn_kubernetes.go
@@ -391,7 +391,7 @@ func renderOVNKubernetes(conf *operv1.NetworkSpec, bootstrapResult *bootstrap.Bo
 		// Less resource constrained clusters can use multiple threads
 		// in northd to improve network operation latency at the cost
 		// of a bit of CPU.
-		data.Data["NorthdThreads"] = 4
+		data.Data["NorthdThreads"] = 1
 	}
 
 	data.Data["OVN_MULTI_NETWORK_ENABLE"] = true
