3276879e2d1341cfecb9980620f65a13d36522d6
Jacob Tanenbaum <jtanenba@redhat.com>
2023-10-24 15:49:33 -0400

    reduce the number of replicas for clusterManager
    in multizone interconnect, both self hosted and managed, only two
    cluster manager pods are required since we no longer run RAFT. Add key
    data.Data["ClusterManagerReplicas"] to be at most two and use it in the
    correct templates

--- pkg/network/ovn_kubernetes.go
+++ pkg/network/ovn_kubernetes.go
@@ -409,8 +409,14 @@ func renderOVNKubernetes(conf *operv1.NetworkSpec, bootstrapResult *bootstrap.Bo
 		data.Data["OVN_MULTI_NETWORK_POLICY_ENABLE"] = true
 	}
 
-	commonManifestDir := filepath.Join(manifestDir, "network/ovn-kubernetes/common")
+	//there only needs to be two cluster managers
+	clusterManagerReplicas := 2
+	if len(bootstrapResult.OVN.MasterAddresses) < 2 {
+		clusterManagerReplicas = len(bootstrapResult.OVN.MasterAddresses)
+	}
+	data.Data["ClusterManagerReplicas"] = clusterManagerReplicas
 
+	commonManifestDir := filepath.Join(manifestDir, "network/ovn-kubernetes/common")
 	cmPaths := []string{
 		filepath.Join(commonManifestDir, "008-script-lib.yaml"),
 	}
