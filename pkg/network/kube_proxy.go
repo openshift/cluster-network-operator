58a70247546e11a7be38e8ddee68d8e3bc058ac3
Tyler Lisowski <lisowski@us.ibm.com>
2024-03-11 12:02:31 -0500

    OCPBUGS-30103: ensure local networking deployments within hypershift use the client side load balancer to be resilient to control plane node failures
    In environments with no cloud load balancer health checking and removing bad control plane endpoints: requests to the control plane from data plane components like kube-proxy and mutlus using the control plane dns name directly will see intermittent failures. In hypershift: a node local haproxy load balancer runs that health checks and removes failed control plane ingress points dynamically. When the components move to using this: control plane failures do not lead to further failures in the data plane starting new workload. fixes: https://issues.redhat.com/browse/OCPBUGS-30103

--- pkg/network/kube_proxy.go
+++ pkg/network/kube_proxy.go
@@ -1,9 +1,12 @@
 package network
 
 import (
+	v1 "github.com/openshift/api/config/v1"
+	"github.com/openshift/cluster-network-operator/pkg/hypershift"
 	"net"
 	"os"
 	"path/filepath"
+	"strconv"
 	"time"
 
 	"github.com/pkg/errors"
@@ -206,8 +209,16 @@ func renderStandaloneKubeProxy(conf *operv1.NetworkSpec, bootstrapResult *bootst
 	data.Data["ReleaseVersion"] = os.Getenv("RELEASE_VERSION")
 	data.Data["KubeProxyImage"] = os.Getenv("KUBE_PROXY_IMAGE")
 	data.Data["KubeRBACProxyImage"] = os.Getenv("KUBE_RBAC_PROXY_IMAGE")
-	data.Data["KUBERNETES_SERVICE_HOST"] = bootstrapResult.Infra.APIServers[bootstrap.APIServerDefault].Host
-	data.Data["KUBERNETES_SERVICE_PORT"] = bootstrapResult.Infra.APIServers[bootstrap.APIServerDefault].Port
+	hsc := hypershift.NewHyperShiftConfig()
+	//TODO (relyt0925): when hypershift appropriately signs kube-apiserver certificate with node local
+	//address it is my recommendation to move to using the node local loadbalancer for all workload
+	if hsc.Enabled && bootstrapResult.Infra.PlatformType == v1.IBMCloudPlatformType {
+		data.Data["KUBERNETES_SERVICE_HOST"] = bootstrapResult.Infra.HostedControlPlane.AdvertiseAddress
+		data.Data["KUBERNETES_SERVICE_PORT"] = strconv.Itoa(bootstrapResult.Infra.HostedControlPlane.AdvertisePort)
+	} else {
+		data.Data["KUBERNETES_SERVICE_HOST"] = bootstrapResult.Infra.APIServers[bootstrap.APIServerDefault].Host
+		data.Data["KUBERNETES_SERVICE_PORT"] = bootstrapResult.Infra.APIServers[bootstrap.APIServerDefault].Port
+	}
 	data.Data["KubeProxyConfig"] = kpc
 	data.Data["MetricsPort"] = metricsPort
 	data.Data["HealthzPort"] = healthzPort
