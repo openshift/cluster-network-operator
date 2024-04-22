0f1cf2c0a8b18e3a4e59c7c1784dc919cf1d6ca2
Jacob Tanenbaum <jtanenba@redhat.com>
2024-04-22 16:20:14 -0400

    temp commit to get CI signal
    runs CI against a custom ovn-kube image built from https://github.com/openshift/ovn-kubernetes/pull/2134

--- pkg/network/ovn_kubernetes.go
+++ pkg/network/ovn_kubernetes.go
@@ -100,7 +100,7 @@ func renderOVNKubernetes(conf *operv1.NetworkSpec, bootstrapResult *bootstrap.Bo
 	// render the manifests on disk
 	data := render.MakeRenderData()
 	data.Data["ReleaseVersion"] = os.Getenv("RELEASE_VERSION")
-	data.Data["OvnImage"] = os.Getenv("OVN_IMAGE")
+	data.Data["OvnImage"] = "quay.io/jtanenba/ovn-kubernetes:release4.12sdn4185"
 	data.Data["OvnkubeMasterReplicas"] = len(bootstrapResult.OVN.MasterAddresses)
 	data.Data["KubeRBACProxyImage"] = os.Getenv("KUBE_RBAC_PROXY_IMAGE")
 	data.Data["Socks5ProxyImage"] = os.Getenv("SOCKS5_PROXY_IMAGE")
