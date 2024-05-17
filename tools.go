c82e06b9709c681b07d3b7542038bffbdaadf2b3
Patryk Diak <pdiak@redhat.com>
2024-05-17 18:25:14 +0200

    Add controller-gen as a dependency
    Steps:
     - Modified tools.go
     - go get sigs.k8s.io/controller-tools/cmd/controller-gen@v0.14.0 - Did not use v0.15.0 since it depends on k8s 1.30
     - go mod tidy; go mod vendor
    
    Signed-off-by: Patryk Diak <pdiak@redhat.com>

--- tools.go
+++ tools.go
@@ -9,4 +9,6 @@ import (
 	_ "github.com/openshift/api/cloudnetwork/v1/zz_generated.crd-manifests"
 	_ "github.com/openshift/api/networkoperator/v1/zz_generated.crd-manifests"
 	_ "github.com/openshift/api/operator/v1/zz_generated.crd-manifests"
+	// dependency of generating CRD for install-config
+	_ "sigs.k8s.io/controller-tools/cmd/controller-gen"
 )
