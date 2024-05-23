05b66b58bb7ad532d4367f70b7fe726cd381b661
Periyasamy Palanisamy <pepalani@redhat.com>
2024-05-23 19:00:23 +0200

    Add machine config watcher for status manager
    This enhances status manager to watch for machine config and machine
    config pool objects, retrieves machine config pool status when ipsec
    is enabled and when any one of the machine config pool goes into
    degraded state, then makes network operator also into degraded state.
    
    Signed-off-by: Periyasamy Palanisamy <pepalani@redhat.com>
    (cherry picked from commit 4753cfa07f3d5a348bc7972fd4a69d3efb98285a)
    (cherry picked from commit 35727700d22ef9c13b76a23ea84e7edf72d689f0)

--- pkg/network/ovn_kubernetes.go
+++ pkg/network/ovn_kubernetes.go
@@ -1455,7 +1455,7 @@ func shouldUpdateOVNKonPrepull(ovn bootstrap.OVNBootstrapResult, releaseVersion 
 func isCNOIPsecMachineConfigPresent(infra bootstrap.InfraStatus) bool {
 	isCNOIPsecMachineConfigPresentIn := func(mcs []*mcfgv1.MachineConfig) bool {
 		for _, mc := range mcs {
-			if containsNetworkOwnerRef(mc.OwnerReferences) {
+			if ContainsNetworkOwnerRef(mc.OwnerReferences) {
 				return true
 			}
 		}
@@ -1463,16 +1463,6 @@ func isCNOIPsecMachineConfigPresent(infra bootstrap.InfraStatus) bool {
 	}
 	return isCNOIPsecMachineConfigPresentIn(infra.MasterIPsecMachineConfigs) &&
 		isCNOIPsecMachineConfigPresentIn(infra.WorkerIPsecMachineConfigs)
-}
-
-func containsNetworkOwnerRef(ownerRefs []metav1.OwnerReference) bool {
-	for _, ownerRef := range ownerRefs {
-		if ownerRef.APIVersion == operv1.GroupVersion.String() && ownerRef.Kind == "Network" &&
-			(ownerRef.Controller != nil && *ownerRef.Controller) && ownerRef.Name == "cluster" {
-			return true
-		}
-	}
-	return false
 }
 
 // isUserDefinedIPsecMachineConfigPresent returns true if user owned MachineConfigs for IPsec
@@ -2005,4 +1995,16 @@ func GetMasqueradeSubnet(conf *operv1.OVNKubernetesConfig) (v4Subnet, v6Subnet s
 		}
 	}
 	return
+}
+
+// ContainsNetworkOwnerRef returns true if any one the given OwnerReference is owned
+// by cluster network operator, otherwise returns false
+func ContainsNetworkOwnerRef(ownerRefs []metav1.OwnerReference) bool {
+	for _, ownerRef := range ownerRefs {
+		if ownerRef.APIVersion == operv1.GroupVersion.String() && ownerRef.Kind == "Network" &&
+			(ownerRef.Controller != nil && *ownerRef.Controller) && ownerRef.Name == "cluster" {
+			return true
+		}
+	}
+	return false
 }
