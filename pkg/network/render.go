440b0cb83244f630c22c01939ee23a0231789b9e
Dan Winship <danwinship@redhat.com>
2024-05-23 12:35:27 -0400

    Drop network type migration support

--- pkg/network/render.go
+++ pkg/network/render.go
@@ -12,7 +12,6 @@ import (
 	"strings"
 
 	configv1 "github.com/openshift/api/config/v1"
-	apifeatures "github.com/openshift/api/features"
 	operv1 "github.com/openshift/api/operator/v1"
 	"github.com/openshift/cluster-network-operator/pkg/bootstrap"
 	cnoclient "github.com/openshift/cluster-network-operator/pkg/client"
