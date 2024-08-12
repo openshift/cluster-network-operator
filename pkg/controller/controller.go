ccbd7590d534431e82356108d7eeb38f69d0dee8
Vadim Rutkovsky <vrutkovs@redhat.com>
2024-08-12 12:46:57 +0200

    Issue shorter certificates when ShortRotation featuregate is enabled

--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -3,16 +3,17 @@ package controller
 import (
 	cnoclient "github.com/openshift/cluster-network-operator/pkg/client"
 	"github.com/openshift/cluster-network-operator/pkg/controller/statusmanager"
+	"github.com/openshift/library-go/pkg/operator/configobserver/featuregates"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 )
 
 // AddToManagerFuncs is a list of functions to add all Controllers to the Manager
-var AddToManagerFuncs []func(manager.Manager, *statusmanager.StatusManager, cnoclient.Client) error
+var AddToManagerFuncs []func(manager.Manager, *statusmanager.StatusManager, cnoclient.Client, featuregates.FeatureGate) error
 
 // AddToManager adds all Controllers to the Manager
-func AddToManager(m manager.Manager, sm *statusmanager.StatusManager, c cnoclient.Client) error {
+func AddToManager(m manager.Manager, sm *statusmanager.StatusManager, c cnoclient.Client, featureGates featuregates.FeatureGate) error {
 	for _, f := range AddToManagerFuncs {
-		if err := f(m, sm, c); err != nil {
+		if err := f(m, sm, c, featureGates); err != nil {
 			return err
 		}
 	}
