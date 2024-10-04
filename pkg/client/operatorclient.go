ec157cfd6dd27b3c7fceedca9e5ef35b3bf15f36
Jamo Luhrsen <jluhrsen@gmail.com>
2024-10-04 13:02:05 -0700

    SDN-5234: rebase k8s v1.31.1
    Update with:
    
    go get -u github.com/openshift/build-machinery-go
    go get -u k8s.io/api
    go get -u k8s.io/apimachinery
    go get -u k8s.io/code-generator
    go get -u k8s.io/component-base
    go get -u k8s.io/klog/v2
    go get -u k8s.io/kube-proxy
    go get -u k8s.io/utils
    go get -u sigs.k8s.io/controller-runtime
    go get -u github.com/openshift/api
    go get -u github.com/openshift/client-go
    go get -u github.com/openshift/library-go
    go get -u github.com/openshift/machine-config-operator
    go get -u k8s.io/apiextensions-apiserver
    go get -u k8s.io/client-go
    go get -u sigs.k8s.io/controller-tools
    go mod tidy
    go mod vendor
    make verify
    
    Build was breaking due to a change in openshift/library-go [0].
    Implemented some new functions for:
    
    genericoperatorclient.NewClusterScopedOperatorClient()
    v1helpers.OperatorClient
    
    [0] https://github.com/openshift/library-go/pull/1787
    
    Signed-off-by: Jamo Luhrsen <jluhrsen@gmail.com>

--- pkg/client/operatorclient.go
+++ pkg/client/operatorclient.go
@@ -2,9 +2,16 @@ package client
 
 import (
 	"context"
-
+	"encoding/json"
+	"fmt"
+	v1 "github.com/openshift/client-go/operator/applyconfigurations/operator/v1"
 	"github.com/openshift/cluster-network-operator/pkg/names"
+	"github.com/openshift/library-go/pkg/apiserver/jsonpatch"
 	"github.com/openshift/library-go/pkg/operator/v1helpers"
+	"k8s.io/apimachinery/pkg/api/equality"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/utils/clock"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/tools/cache"
@@ -19,6 +26,7 @@ import (
 type OperatorHelperClient struct {
 	informer operatorinformerv1.NetworkInformer
 	client   operatorclientv1.NetworkInterface
+	clock    clock.RealClock
 }
 
 // OperatorHelperClient implements the v1helpers OperatorClient interface
@@ -80,10 +88,129 @@ func (c *OperatorHelperClient) UpdateOperatorStatus(ctx context.Context, resourc
 	updated.ResourceVersion = resourceVersion
 	updated.Status.OperatorStatus = *status
 
-	ret, err := c.client.Update(ctx, updated, metav1.UpdateOptions{})
+	ret, err := c.client.UpdateStatus(ctx, updated, metav1.UpdateOptions{})
 	if err != nil {
 		return nil, err
 	}
 
 	return &ret.Status.OperatorStatus, nil
+}
+
+func (c *OperatorHelperClient) ApplyOperatorSpec(ctx context.Context, fieldManager string, desiredConfiguration *v1.OperatorSpecApplyConfiguration) error {
+	if desiredConfiguration == nil {
+		return fmt.Errorf("desiredConfiguration must have a value")
+	}
+
+	desiredSpec := &v1.NetworkSpecApplyConfiguration{
+		OperatorSpecApplyConfiguration: *desiredConfiguration,
+	}
+	desired := v1.Network(names.CLUSTER_CONFIG)
+	desired.WithSpec(desiredSpec)
+
+	instance, err := c.client.Get(ctx, names.CLUSTER_CONFIG, metav1.GetOptions{})
+	switch {
+	case apierrors.IsNotFound(err):
+	case err != nil:
+		return fmt.Errorf("unable to get operator configuration: %w", err)
+	default:
+		original, err := v1.ExtractNetwork(instance, fieldManager)
+		if err != nil {
+			return fmt.Errorf("unable to extract network operator configuration: %w", err)
+		}
+		if equality.Semantic.DeepEqual(original, desired) {
+			return nil
+		}
+	}
+
+	_, err = c.client.Apply(ctx, desired, metav1.ApplyOptions{
+		Force:        true,
+		FieldManager: fieldManager,
+	})
+	if err != nil {
+		return fmt.Errorf("unable to Apply network operator using fieldManager %q: %w", fieldManager, err)
+	}
+
+	return nil
+}
+
+func (c *OperatorHelperClient) ApplyOperatorStatus(ctx context.Context, fieldManager string, desiredStatus *v1.OperatorStatusApplyConfiguration) error {
+	if desiredStatus == nil {
+		return fmt.Errorf("desiredStatus must have a value")
+	}
+
+	desired := v1.Network(names.CLUSTER_CONFIG)
+	instance, err := c.client.Get(ctx, names.CLUSTER_CONFIG, metav1.GetOptions{})
+	switch {
+	case apierrors.IsNotFound(err):
+		v1helpers.SetApplyConditionsLastTransitionTime(c.clock, &desiredStatus.Conditions, nil)
+		desiredStatus := &v1.NetworkStatusApplyConfiguration{
+			OperatorStatusApplyConfiguration: *desiredStatus,
+		}
+		desired.WithStatus(desiredStatus)
+	case err != nil:
+		return fmt.Errorf("unable to get network operator configuration: %w", err)
+	default:
+		previous, err := v1.ExtractNetworkStatus(instance, fieldManager)
+		if err != nil {
+			return fmt.Errorf("unable to extract network operator configuration: %w", err)
+		}
+
+		var operatorStatus *v1.OperatorStatusApplyConfiguration
+		if previous.Status != nil {
+			operatorStatus = &v1.OperatorStatusApplyConfiguration{}
+			jsonBytes, err := json.Marshal(previous.Status)
+			if err != nil {
+				return fmt.Errorf("unable to serialize network operator configuration: %w", err)
+			}
+			if err := json.Unmarshal(jsonBytes, operatorStatus); err != nil {
+				return fmt.Errorf("unable to deserialize network operator configuration: %w", err)
+			}
+		}
+
+		switch {
+		case desiredStatus.Conditions != nil && operatorStatus != nil:
+			v1helpers.SetApplyConditionsLastTransitionTime(c.clock, &desiredStatus.Conditions, operatorStatus.Conditions)
+		case desiredStatus.Conditions != nil && operatorStatus == nil:
+			v1helpers.SetApplyConditionsLastTransitionTime(c.clock, &desiredStatus.Conditions, nil)
+		}
+
+		v1helpers.CanonicalizeOperatorStatus(desiredStatus)
+		v1helpers.CanonicalizeOperatorStatus(operatorStatus)
+
+		original := v1.Network(names.CLUSTER_CONFIG)
+		if operatorStatus != nil {
+			originalStatus := &v1.NetworkStatusApplyConfiguration{
+				OperatorStatusApplyConfiguration: *operatorStatus,
+			}
+			original.WithStatus(originalStatus)
+		}
+
+		desiredStatus := &v1.NetworkStatusApplyConfiguration{
+			OperatorStatusApplyConfiguration: *desiredStatus,
+		}
+		desired.WithStatus(desiredStatus)
+
+		if equality.Semantic.DeepEqual(original, desired) {
+			return nil
+		}
+	}
+
+	_, err = c.client.ApplyStatus(ctx, desired, metav1.ApplyOptions{
+		Force:        true,
+		FieldManager: fieldManager,
+	})
+	if err != nil {
+		return fmt.Errorf("unable to Apply Status for network operator using fieldManager %q: %w", fieldManager, err)
+	}
+
+	return nil
+}
+
+func (c *OperatorHelperClient) PatchOperatorStatus(ctx context.Context, jsonPatch *jsonpatch.PatchSet) (err error) {
+	jsonPatchBytes, err := jsonPatch.Marshal()
+	if err != nil {
+		return err
+	}
+	_, err = c.client.Patch(ctx, names.OPERATOR_CONFIG, types.JSONPatchType, jsonPatchBytes, metav1.PatchOptions{}, "/status")
+	return err
 }
