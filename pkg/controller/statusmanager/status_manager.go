5762c319eddcffc38167101ebb0bc23ca34c6c01
Periyasamy Palanisamy <pepalani@redhat.com>
2024-12-10 15:51:00 +0100

    Use network config last seen annotation for machine config
    When network operator owned machine config is deleted, though mco is still deleting
    the rendered machine config from the machine config pool, there is no way to lookup
    owner reference on the machine config object upon network operator pod reboot caused
    by node reboot. This commit writes machine config rendering status onto
    network.operator.openshift.io/last-seen-machineconfig-state network co annotation
    and this is being looked up to report status accordingly.
    
    Signed-off-by: Periyasamy Palanisamy <pepalani@redhat.com>
    (cherry picked from commit 080f4455c4110e842d01b87518ae0739f01ad42e)
    (cherry picked from commit 79e7d3ea3184cf2739f5e71bd1dceb2cc7fce2d9)

--- pkg/controller/statusmanager/status_manager.go
+++ pkg/controller/statusmanager/status_manager.go
@@ -29,6 +29,7 @@ import (
 	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/util/sets"
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/util/retry"
 	"k8s.io/klog/v2"
@@ -104,11 +105,10 @@ type StatusManager struct {
 
 	relatedObjects []configv1.ObjectReference
 
-	// local cache to store deployed network operator machine configs.
-	availableMachineConfigs map[string]sets.Set[string]
-	// local cache to store deleted network operator machine configs and
-	// not yet removed from corresponding machine config pool.
-	deletedMachineConfigs map[string]sets.Set[string]
+	// local cache to store rendered network operator machine configs.
+	renderedMachineConfigs map[string]sets.Set[string]
+	// local cache to store network operator machine configs being deleted.
+	machineConfigsBeingRemoved map[string]sets.Set[string]
 
 	// used only for upgrades from <=4.13 to 4.14 with ovn-kubernetes
 	// TODO: remove in 4.15
@@ -121,14 +121,13 @@ func New(client cnoclient.Client, name, cluster string) *StatusManager {
 		name:             name,
 		hyperShiftConfig: hypershift.NewHyperShiftConfig(),
 
-		dsInformers:             map[string]cache.SharedIndexInformer{},
-		dsListers:               map[string]DaemonSetLister{},
-		depInformers:            map[string]cache.SharedIndexInformer{},
-		depListers:              map[string]DeploymentLister{},
-		ssInformers:             map[string]cache.SharedIndexInformer{},
-		ssListers:               map[string]StatefulSetLister{},
-		availableMachineConfigs: map[string]sets.Set[string]{},
-		deletedMachineConfigs:   map[string]sets.Set[string]{},
+		dsInformers:                map[string]cache.SharedIndexInformer{},
+		dsListers:                  map[string]DaemonSetLister{},
+		depInformers:               map[string]cache.SharedIndexInformer{},
+		depListers:                 map[string]DeploymentLister{},
+		ssInformers:                map[string]cache.SharedIndexInformer{},
+		ssListers:                  map[string]StatefulSetLister{},
+		machineConfigsBeingRemoved: map[string]sets.Set[string]{},
 	}
 	var err error
 	status.labelSelector, err = labels.Parse(fmt.Sprintf("%s==%s", names.GenerateStatusLabel, cluster))
