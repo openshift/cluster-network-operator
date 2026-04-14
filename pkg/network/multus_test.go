f65e0aed0378b25d26f249a65a827cb5473db033
Evan Hearne <ehearne@redhat.com>
2026-04-14 15:46:56 +0100

    Add dedicated service account for cni-sysctl-allowlist-ds
    The cni-sysctl-allowlist-ds pod was using the default service account.
    Add serviceAccountName: multus-ancillary-tools to use a dedicated SA.
    
    Also make sure that defaultt SA isn't used when multus network node
    identity is enabled.
    
    This work was taken from yboaron's PR in #2845. yboaron has given me permission to
    take over his work on this.
    
    Signed-off-by: Evan Hearne <ehearne@redhat.com>

--- pkg/network/multus_test.go
+++ pkg/network/multus_test.go
@@ -1,11 +1,14 @@
 package network
 
 import (
+	"path/filepath"
 	"testing"
 
 	operv1 "github.com/openshift/api/operator/v1"
 
 	. "github.com/onsi/gomega"
+	"github.com/openshift/cluster-network-operator/pkg/render"
+	uns "k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 )
 
 var MultusConfig = operv1.Network{
@@ -55,4 +58,85 @@ func TestRenderMultus(t *testing.T) {
 	g.Expect(objs).To(ContainElement(HaveKubernetesID("ClusterRole", "", "multus")))
 	g.Expect(objs).To(ContainElement(HaveKubernetesID("DaemonSet", "openshift-multus", "multus")))
 	g.Expect(objs).To(ContainElement(HaveKubernetesID("NetworkPolicy", "openshift-multus", "default-deny")))
+}
+
+// TestMultusServiceAccountWithoutNodeIdentity tests service account is set when node identity is disabled
+func TestMultusServiceAccountWithoutNodeIdentity(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	crd := MultusConfig.DeepCopy()
+	config := &crd.Spec
+	enabled := false
+	config.DisableMultiNetwork = &enabled
+	fillDefaults(config, nil)
+
+	// Test without node identity - should have service account
+	bootstrapWithoutNodeIdentity := fakeBootstrapResult()
+	bootstrapWithoutNodeIdentity.Infra.NetworkNodeIdentityEnabled = false
+
+	objs, err := renderMultus(config, bootstrapWithoutNodeIdentity, manifestDir)
+	g.Expect(err).NotTo(HaveOccurred())
+
+	daemonSet := findDaemonSet(objs, "openshift-multus", "multus")
+	g.Expect(daemonSet).NotTo(BeNil())
+
+	serviceAccount, found, err := uns.NestedString(daemonSet.Object, "spec", "template", "spec", "serviceAccountName")
+	g.Expect(err).NotTo(HaveOccurred())
+	g.Expect(found).To(BeTrue())
+	g.Expect(serviceAccount).To(Equal("multus"))
+
+	// Verify that multus-node-identity service account is NOT created when node identity is disabled
+	g.Expect(objs).NotTo(ContainElement(HaveKubernetesID("ServiceAccount", "openshift-multus", "multus-node-identity")))
+
+	// Test with node identity - should use multus-node-identity service account
+	bootstrapWithNodeIdentity := fakeBootstrapResult()
+	bootstrapWithNodeIdentity.Infra.NetworkNodeIdentityEnabled = true
+
+	objs, err = renderMultus(config, bootstrapWithNodeIdentity, manifestDir)
+	g.Expect(err).NotTo(HaveOccurred())
+
+	daemonSet = findDaemonSet(objs, "openshift-multus", "multus")
+	g.Expect(daemonSet).NotTo(BeNil())
+
+	serviceAccount, found, err = uns.NestedString(daemonSet.Object, "spec", "template", "spec", "serviceAccountName")
+	g.Expect(err).NotTo(HaveOccurred())
+	g.Expect(found).To(BeTrue(), "serviceAccountName should be set when NETWORK_NODE_IDENTITY_ENABLE=true")
+	g.Expect(serviceAccount).To(Equal("multus-node-identity"), "daemonset should use multus-node-identity service account when node identity is enabled")
+
+	// Verify that multus-node-identity service account is created when node identity is enabled
+	g.Expect(objs).To(ContainElement(HaveKubernetesID("ServiceAccount", "openshift-multus", "multus-node-identity")))
+}
+
+func findDaemonSet(objs []*uns.Unstructured, namespace, name string) *uns.Unstructured {
+	for _, obj := range objs {
+		if obj.GetKind() == "DaemonSet" && obj.GetNamespace() == namespace && obj.GetName() == name {
+			return obj
+		}
+	}
+	return nil
+}
+
+// TestAllowlistDaemonSetServiceAccount verifies that cni-sysctl-allowlist-ds is using its bespoke service account
+func TestAllowlistDaemonSetServiceAccount(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	// Render the allowlist daemonset manifests
+	data := render.MakeRenderData()
+	data.Data["MultusImage"] = "test-multus-image:latest"
+	data.Data["CniSysctlAllowlist"] = "cni-sysctl-allowlist"
+	data.Data["ReleaseVersion"] = "test-version"
+
+	objs, err := render.RenderDir(filepath.Join(manifestDir, "allowlist/daemonset"), &data)
+	g.Expect(err).NotTo(HaveOccurred())
+	g.Expect(objs).NotTo(BeEmpty())
+
+	// Find the cni-sysctl-allowlist-ds daemonset
+	daemonSet := findDaemonSet(objs, "openshift-multus", "cni-sysctl-allowlist-ds")
+	g.Expect(daemonSet).NotTo(BeNil(), "cni-sysctl-allowlist-ds daemonset should be rendered")
+
+	// Verify that the serviceAccountName is set to multus-ancillary-tools
+	serviceAccount, found, err := uns.NestedString(daemonSet.Object, "spec", "template", "spec", "serviceAccountName")
+	g.Expect(err).NotTo(HaveOccurred())
+	g.Expect(found).To(BeTrue(), "serviceAccountName should be set in the daemonset")
+	g.Expect(serviceAccount).To(Equal("multus-ancillary-tools"), "daemonset should use multus-ancillary-tools service account")
 }
