c617131b92f33d65a9c0938cc111a6a7821ccea4
Tom Pantelis <tompantelis@gmail.com>
2026-07-17 08:23:07 -0400

    Add test utility functions for component rendering tests
    This commit adds helper functions to testutil_test.go that are used
    by component rendering tests to verify rendered Kubernetes objects:
    
    - mustFindRenderedObj: Finds and converts unstructured objects using generics
    - mustFindContainer: Finds a container by name in a container list
    - findExecCommand: Extracts exec command strings from container command args
    
    These utilities facilitate testing of rendered manifests and container
    configurations across multiple components.
    
    Signed-off-by: Tom Pantelis <tompantelis@gmail.com>

--- pkg/network/testutil_test.go
+++ pkg/network/testutil_test.go
@@ -3,14 +3,20 @@ package network
 import (
 	"context"
 	"fmt"
+	"slices"
+	"strings"
+	"testing"
 
+	. "github.com/onsi/gomega"
 	"github.com/onsi/gomega/types"
 	configv1 "github.com/openshift/api/config/v1"
 	"github.com/openshift/cluster-network-operator/pkg/bootstrap"
 	"github.com/openshift/cluster-network-operator/pkg/client"
 	"github.com/openshift/cluster-network-operator/pkg/hypershift"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	uns "k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
 )
 
 // Fun matcher for testing the presence of Kubernetes objects
@@ -107,4 +113,47 @@ func createProxy(client client.Client) error {
 		},
 	}
 	return client.Default().CRClient().Create(context.TODO(), proxy)
+}
+
+// mustFindRenderedObj finds and converts an unstructured object from a list of rendered objects.
+// It uses Go generics to return the properly typed object.
+func mustFindRenderedObj[T any](t *testing.T, objs []*uns.Unstructured, kind, name string) T {
+	t.Helper()
+	g := NewWithT(t)
+
+	index := slices.IndexFunc(objs, func(obj *uns.Unstructured) bool {
+		return obj.GetKind() == kind && obj.GetName() == name
+	})
+	g.Expect(index).NotTo(Equal(-1), "Could not find object with kind %q and name %q", kind, name)
+
+	var result T
+	err := runtime.DefaultUnstructuredConverter.FromUnstructured(objs[index].Object, &result)
+	g.Expect(err).NotTo(HaveOccurred())
+
+	return result
+}
+
+// mustFindContainer finds a container by name in a list of containers.
+func mustFindContainer(t *testing.T, in []corev1.Container, name string) *corev1.Container {
+	t.Helper()
+	g := NewWithT(t)
+
+	c, ok := findContainer(in, name)
+	g.Expect(ok).To(BeTrue(), "Could not find container with name %q", name)
+
+	return &c
+}
+
+// findExecCommand finds and returns the exec command for the given binary in container command args.
+// It expects cmdArgs to have length 3 (bash, -c, script) and returns the portion from "exec /usr/bin/<binary>" onwards.
+func findExecCommand(t *testing.T, cmdArgs []string, binaryName string) string {
+	t.Helper()
+	g := NewWithT(t)
+
+	g.Expect(cmdArgs).To(HaveLen(3))
+	execPattern := "exec /usr/bin/" + binaryName
+	startIdx := strings.Index(cmdArgs[2], execPattern)
+	g.Expect(startIdx).NotTo(Equal(-1), "Could not find '%s' in command args: %q", execPattern, cmdArgs[2])
+
+	return cmdArgs[2][startIdx:]
 }
