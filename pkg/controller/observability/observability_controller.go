c7702f9d0687c7a3acdee2a2c09327f3f41fbc21
Kapil Jain <kapjain@redhat.com>
2026-08-13 13:16:25 -0400

    namespace

--- pkg/controller/observability/observability_controller.go
+++ pkg/controller/observability/observability_controller.go
@@ -472,8 +472,8 @@ func (r *ReconcileObservability) checkOLMv0Installation(ctx context.Context) (bo
 	// Look for netobserv operator CSV
 	for _, item := range csvList.Items {
 		name := item.GetName()
-		// CSV name for OLMv0 is like this: network-observability-operator.v1.2.3
-		if strings.HasPrefix(name, "network-observability-operator") {
+		// CSV name for OLMv0 is like this: network-observability-operator.v1.2.3 or netobserv-operator.v0.0.0-sha-xxx
+		if strings.HasPrefix(name, "network-observability-operator") || strings.HasPrefix(name, "netobserv-operator") {
 			// Check the CSV phase
 			phase, found, err := unstructured.NestedString(item.Object, "status", "phase")
 			if err != nil {
